44f63093e8485c5029be7364efa69f1c7a12344d
Dream-Master <dream-master@gmx.net>
2020-04-05 23:02:42 +0200

    commented BW ore mixes out because the dont work rn.

--- src/main/java/com/dreammaster/bartworksHandler/BW_New_Ores_Loader.java
+++ src/main/java/com/dreammaster/bartworksHandler/BW_New_Ores_Loader.java
@@ -1,4 +1,4 @@
-package com.dreammaster.bartworksHandler;
+/**package com.dreammaster.bartworksHandler;
 
 import bloodasp.galacticgreg.bartworks.BW_Worldgen_Ore_Layer_Space;
 import bloodasp.galacticgreg.bartworks.BW_Worldgen_Ore_SmallOre_Space;
@@ -16,6 +16,7 @@ public class BW_New_Ores_Loader implements Runnable {
     /**
      * Small Ores go in here
      */
+/**
     private void loadSmallOres(){
         new BW_Worldgen_Ore_SmallOre_Space("small.Platinum",true,20,80,8, PTMetallicPowder.getmID(),PTMetallicPowder);
         new BW_Worldgen_Ore_SmallOre_Space("small.Osmium",true,10,30,8, IrOsLeachResidue.getmID(),IrOsLeachResidue);
@@ -28,8 +29,11 @@ public class BW_New_Ores_Loader implements Runnable {
     /**
      * Ore Veins go in here
      */
+/**
     private void loadVeins(){
         new BW_Worldgen_Ore_Layer_Space("mix.mercury",true,20,60,25,3,16, Cinnabar,Terlinguaite,Atheneite,Temagamite);
     }
 
 }
+
+        */
