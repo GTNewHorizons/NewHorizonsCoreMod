56b0e48c5c93cdf98f7259016b3853b1bea9fe65
DreamMasterXXL <dream-master@gmx.net>
2020-04-23 21:24:33 +0200

    UnknownNutrientAgar takes too long (Biowares) #5964 https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues/5964

--- src/main/java/com/dreammaster/bartworksHandler/BioItemLoader.java
+++ src/main/java/com/dreammaster/bartworksHandler/BioItemLoader.java
@@ -109,7 +109,7 @@ public class BioItemLoader {
                         GT_ModHandler.getModItem("harvestcraft","seaweedItem",64)
                 },
                 CultureSet.get("TcetiEBac"),
-                new FluidStack[]{new FluidStack(BIOFLUIDS[2],1600)},
+                new FluidStack[]{new FluidStack(BIOFLUIDS[2],50)},
                 new FluidStack[]{new FluidStack(BIOFLUIDS[3],50)},
                 1200,BW_Util.getMachineVoltageFromTier(8),100,8,0,false
         );
