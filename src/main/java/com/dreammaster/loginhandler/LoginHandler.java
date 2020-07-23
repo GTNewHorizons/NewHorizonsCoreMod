774069cf4fbe2e17a074f43d11960d93b7cde7bc
DreamMasterXXL <dream-master@gmx.net>
2020-07-23 14:50:51 +0200

    info(Core Mod) add info Text add Quest Book info text to starting menu

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -13,7 +13,8 @@ public class LoginHandler
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event)
     {
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=================================================="));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD + "Welcome to Gregtech:New Horizons " + EnumChatFormatting.GREEN + CoreModConfig.ModPackVersion) );
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD + "Welcome to Gregtech:New Horizons " + EnumChatFormatting.GREEN + CoreModConfig.ModPackVersion));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Quest Book have a short key.Set it up in the controls menu." ));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Please report bugs here: https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues" ));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BLUE + "Please bring comments to https://discord.gg/EXshrPV" ));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=================================================="));
