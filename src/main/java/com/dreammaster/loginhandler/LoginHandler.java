cc2432a9120de503ba17bd18e1676f73981b1ad6
DreamMasterXXL <dream-master@gmx.net>
2020-07-24 13:44:36 +0200

    fix(Core Mod) update info Text again update Quest Book info text in starting menu

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -12,11 +12,13 @@ public class LoginHandler
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event)
     {
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "================================================="));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=================================================" ));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD + "Welcome to Gregtech:New Horizons " + EnumChatFormatting.GREEN + CoreModConfig.ModPackVersion));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "The Quest Book has a shortcut key, " + "check your controls to set it up correctly." ));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Please report bugs here: " + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues" ));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "The Quest Book has a shortcut key," ));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "check your controls to set it up correctly." ));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Please report bugs here:" ));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues" ));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BLUE + "Please bring comments to https://discord.gg/EXshrPV" ));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=================================================="));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "==================================================" ));
     }
 }
