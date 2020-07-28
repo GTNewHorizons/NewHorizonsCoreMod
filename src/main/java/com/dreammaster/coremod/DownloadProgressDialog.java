14eea589e9ffc92afebc8a658c2f7427f84e74ff
llk89 <27812632+llk89@users.noreply.github.com>
2020-07-29 05:49:16 +0800

    Add deploader functionality
    1. Load stuff to download from config/dependencies.json. Format is as below.
    
        [
            {
                "path": "mods/AngerMod-1.7.10-0.6.jar",
                "url": "http://downloads.gtnewhorizons.com/Mods_for_Twitch/AngerMod-1.7.10-0.6.jar"
            },
            {
                "path": "mods/1.7.10/Baubles-1.7.10-1.0.1.12.jar",
                "url": "http://downloads.gtnewhorizons.com/Mods_for_Twitch/Baubles-1.7.10-1.0.1.12.jar"
            }
        ]
    
    2. Do nothing if
      1. dependencies.json is not present
      2. dependencies.json is empty
      3. all dependencies specified are present
    
    3. If a previous download fails half way, then already downloaded files will not be downloaded again, but the one that fails will have to restart from scratch.
    
    4. Destination directories will be automatically created if it is not present
    
    5. Destination directories are relative to .minecraft directory, e.g. to place stuff in mods folder you put `mods/MyMod-1.0.jar` into `path`.
    
    6. Make sure dependencies.json is a valid JSON file. Try an online JSON validator if you are not sure about that.
    
    Signed-off-by: llk89 <27812632+llk89@users.noreply.github.com>

---
+++ src/main/java/com/dreammaster/coremod/DownloadProgressDialog.java
@@ -0,0 +1,64 @@
+package com.dreammaster.coremod;
+
+import java.awt.*;
+import java.awt.event.WindowEvent;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+class DownloadProgressDialog extends JDialog {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 6041491111144915139L;
+    public static final String WINDOW_TITLE = "GregTech: New Horizon";
+    private Thread netThread;
+    private JProgressBar progressBar;
+
+    public void setJobCount(int max) {
+        progressBar.setMaximum(max);
+    }
+
+    public void progress() {
+        progressBar.setValue(progressBar.getValue() + 1);
+    }
+
+    /**
+     * Create the dialog.
+     */
+    public DownloadProgressDialog() {
+        setTitle(WINDOW_TITLE);
+        setBounds(100, 100, 450, 172);
+        getContentPane().setLayout(new BorderLayout());
+        JPanel contentPanel = new JPanel();
+        contentPanel.setBorder(new EmptyBorder(15, 15, 15, 15));
+        getContentPane().add(contentPanel, BorderLayout.CENTER);
+        contentPanel.setLayout(new BorderLayout(0, 0));
+        {
+            JLabel lblNewLabel = new JLabel("Downloading additional files");
+            lblNewLabel.setFont(lblNewLabel.getFont().deriveFont(24.0f));
+            lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            contentPanel.add(lblNewLabel, BorderLayout.NORTH);
+        }
+        {
+            JPanel buttonPane = new JPanel();
+            contentPanel.add(buttonPane, BorderLayout.SOUTH);
+            buttonPane.setLayout(new FlowLayout(FlowLayout.CENTER));
+            {
+                JButton cancelButton = new JButton("Cancel");
+                cancelButton.addActionListener(e -> DownloadProgressDialog.this.dispatchEvent(new WindowEvent(DownloadProgressDialog.this, WindowEvent.WINDOW_CLOSING)));
+                buttonPane.add(cancelButton);
+            }
+        }
+        {
+            JPanel panel = new JPanel();
+            contentPanel.add(panel, BorderLayout.CENTER);
+            panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+            {
+                progressBar = new JProgressBar();
+                progressBar.setStringPainted(true);
+                panel.add(progressBar);
+            }
+        }
+    }
+}
