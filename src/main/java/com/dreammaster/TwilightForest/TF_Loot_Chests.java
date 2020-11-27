4c05bf53b9bd43a5804ed14cb563941f31af61d0
DreamMasterXXL <dream-master@gmx.net>
2020-11-27 18:47:27 +0100

    add test loot

--- src/main/java/com/dreammaster/TwilightForest/TF_Loot_Chests.java
+++ src/main/java/com/dreammaster/TwilightForest/TF_Loot_Chests.java
@@ -8,8 +8,23 @@ public class TF_Loot_Chests {
 
     public static void init(FMLPreInitializationEvent event) {
 
-        TFTreasure.hill1.common.add(GT_ModHandler.getModItem("harvestcraft", "crayfishrawItem", 1, 0), 10);
-        //TFTreasure.hill1.common.add(null, 10);;
+        TFTreasure.hill1.useless.add(GT_ModHandler.getModItem("harvestcraft", "crayfishrawItem", 10, 0), 100);
+        TFTreasure.hill1.common.add(GT_ModHandler.getModItem("harvestcraft", "crayfishrawItem", 10, 0), 70);
+        TFTreasure.hill1.uncommon.add(GT_ModHandler.getModItem("harvestcraft", "crayfishrawItem", 10, 0), 50);
+        TFTreasure.hill1.rare.add(GT_ModHandler.getModItem("harvestcraft", "crayfishrawItem", 10, 0), 20);
+        TFTreasure.hill1.ultrarare.add(GT_ModHandler.getModItem("harvestcraft", "crayfishrawItem", 10, 0), 10);
+
+        TFTreasure.hill1.useless.add(GT_ModHandler.getModItem("harvestcraft", "frograwItem", 10, 0), 100);
+        TFTreasure.hill2.common.add(GT_ModHandler.getModItem("harvestcraft", "frograwItem", 10, 0), 70);
+        TFTreasure.hill2.uncommon.add(GT_ModHandler.getModItem("harvestcraft", "frograwItem", 10, 0), 50);
+        TFTreasure.hill2.rare.add(GT_ModHandler.getModItem("harvestcraft", "frograwItem", 10, 0), 20);
+        TFTreasure.hill2.ultrarare.add(GT_ModHandler.getModItem("harvestcraft", "frograwItem", 10, 0), 10);
+
+        TFTreasure.hill3.useless.add(GT_ModHandler.getModItem("harvestcraft", "shrimprawItem", 10, 0), 100);
+        TFTreasure.hill3.common.add(GT_ModHandler.getModItem("harvestcraft", "shrimprawItem", 10, 0), 70);
+        TFTreasure.hill3.uncommon.add(GT_ModHandler.getModItem("harvestcraft", "shrimprawItem", 10, 0), 50);
+        TFTreasure.hill3.rare.add(GT_ModHandler.getModItem("harvestcraft", "shrimprawItem", 10, 0), 20);
+        TFTreasure.hill3.ultrarare.add(GT_ModHandler.getModItem("harvestcraft", "shrimprawItem", 10, 0), 10);
 
     }
 }
