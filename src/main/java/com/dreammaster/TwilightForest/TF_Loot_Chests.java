8b3e4067ec0b70022182e02f5f4aa10cc3a962bd
DreamMasterXXL <dream-master@gmx.net>
2020-11-27 12:33:09 +0100

    Add new chest loot to tf chests
    (cherry picked from commit 7d7e74782b7fb9a1e479a1af2f0a05d78f846ae0)

---
+++ src/main/java/com/dreammaster/TwilightForest/TF_Loot_Chests.java
@@ -0,0 +1,10 @@
+package com.dreammaster.TwilightForest;
+
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+public class Loot_Chests {
+
+    public static void init(FMLPreInitializationEvent event) {
+
+    }
+}
