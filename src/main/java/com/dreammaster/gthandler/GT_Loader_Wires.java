5d13f8e4babec678d9ed5ba47b36c266d1c8867d
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2021-03-19 01:04:29 +0100

    add new coil recipes and streamline old ones (#163)
    * add new coil recipes and streamline old ones
    
    This was done due to uncraftability of Trinium and HSSS coils. Also, some recipes werent in line with other coil recipes.
    
    * adds missing wires aswell
    
    * oops

--- src/main/java/com/dreammaster/gthandler/GT_Loader_Wires.java
+++ src/main/java/com/dreammaster/gthandler/GT_Loader_Wires.java
@@ -37,6 +37,8 @@ public class GT_Loader_Wires
         makeWires(Materials.BlackPlutonium, 11390, bEC ? 4L : 16L, bEC ? 16L : 32L, 1L, GT_Values.V[13], false, false);
         makeWires(Materials.DraconiumAwakened, 11410, bEC ? 4L : 16L, bEC ? 16L : 32L, 1L, GT_Values.V[14], false, false);
         makeWires(Materials.Infinity, 11430, 1L, 1L, 8192L, GT_Values.V[15], false, true);
+        makeWires(Materials.Trinium, 11450, bEC ? 4L : 16L, bEC ? 8L : 32L, 6L, GT_Values.V[7], true, false);
+        makeWires(Materials.HSSS, 11470, bEC ? 4L : 16L, bEC ? 8L : 32L, 6L, GT_Values.V[9], true, false);
 
         /** ID SPACE RESERVED: to 11800, FOR WIRES
         makeWires(Materials.???, 1920, 4L, 8L, 8L, gregtech.api.enums.GT_Values.V[8], true, false);
