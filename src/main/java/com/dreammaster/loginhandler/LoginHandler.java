59ccab72521df4f6249ecf5066837cf61c0276ab
Prometheus0000 <prometheus0000000@gmail.com>
2021-05-24 17:54:17 -0400

    Minor fixes

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -18,12 +18,12 @@ public class LoginHandler {
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN	+ "I/O, NEI having tabs at the top and multiple recipes on a "));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "page, Cooking for Blockheads addition, Logistics Pipes"));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "addition, Tungsten change, glowy textures (your resourcepack"));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN	+ "will need to be updated to do this), other texture work you "));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN	+ "will need to be updated to do this), other texture work you"));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "probably won't notice but still looks nice, quest changes, and a"));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_GREEN + "whole bunch of other stuff, so check out the changelog."));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GREEN + "Please report bugs here:" ));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/" ));
-        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BLUE + "Visit out discord at https://discord.gg/EXshrPV" ));
+        event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.BLUE + "Visit our discord at https://discord.gg/EXshrPV" ));
         event.player.addChatMessage(new ChatComponentText(EnumChatFormatting.GOLD + "=====================================================" ));
     }
 }
