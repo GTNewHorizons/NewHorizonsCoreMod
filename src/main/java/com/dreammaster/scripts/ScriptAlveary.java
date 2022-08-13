63bd47c2fb51765dc0284ff2611d8d354b30ce08
Runakai1 <Runatson@gmail.com>
2022-08-13 19:24:41 +0200

    Moved industrial Apiary to Coremod

---
+++ src/main/java/com/dreammaster/scripts/ScriptAlveary.java
@@ -0,0 +1,35 @@
+package com.dreammaster.scripts;
+import gregtech.api.enums.GT_Values;
+import gregtech.api.enums.ItemList;
+import net.minecraft.item.ItemStack;
+import scala.actors.threadpool.Arrays;
+
+import static gregtech.api.util.GT_ModHandler.getModItem;
+
+public class ScriptAlveary implements IScriptLoader{
+
+    public ScriptAlveary(){
+
+    }
+
+    @Override
+    public void initScriptData(){
+        scriptName.setLength(0);
+        scriptName.append("Alveary");
+        dependencies.clear();
+        dependencies.addAll(java.util.Arrays.asList("Forestry", "gregtech", "gendustry", "ExtraBees"));
+    }
+
+    public static ItemStack Alveary_1 = getModItem("ExtraBees", "alveary", 1, 1);
+    public static ItemStack Alveary_3 = getModItem("ExtraBees", "alveary", 1, 3);
+    public static ItemStack Alveary_7 = getModItem("Forestry", "alveary", 1, 7);
+    public static ItemStack Chipset_1 = getModItem("Forestry", "chipsets", 1, 1);
+
+    @Override
+    public void loadRecipes() {
+        addShapedRecipe(getModItem("gregtech", "gt.blockmachines", 1, 9399), new Object[]{
+                Alveary_1, Chipset_1, Alveary_1,
+                ItemList.Robot_Arm_HV.get(1), ItemList.FR_Casing_Sturdy.get(1),  ItemList.Robot_Arm_HV.get(1),
+                Alveary_3, Alveary_7, Alveary_3});
+    }
+}
