5655f809f519cd2eceb238213cdca774079befa0
Runakai1 <Runatson@gmail.com>
2022-08-16 18:59:23 +0200

    Dependencies gone, ModItem, Rework

--- src/main/java/com/dreammaster/scripts/ScriptAE2FC.java
+++ src/main/java/com/dreammaster/scripts/ScriptAE2FC.java
@@ -12,15 +12,9 @@ import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.ShapedOreRecipe;
-import net.minecraftforge.oredict.ShapelessOreRecipe;
-import com.glodblock.github.common.Config;
-import com.glodblock.github.common.item.ItemBasicFluidStorageCell;
 import com.glodblock.github.common.storage.CellType;
-import com.glodblock.github.util.ModAndClassUtil;
 
-import static com.glodblock.github.loader.ItemAndBlockHolder.*;
 import static gregtech.api.util.GT_ModHandler.getModItem;
 
 public class ScriptAE2FC implements IScriptLoader {
@@ -47,7 +41,6 @@ public class ScriptAE2FC implements IScriptLoader {
         final ItemStack AE2_WORK_BENCH = getModItem("appliedenergistics2", "tile.BlockCellWorkbench", 1);
         final ItemStack AE2_PATTERN_TERM = getModItem("appliedenergistics2", "item.ItemMultiPart", 1, 340);
         final ItemStack AE2_PROCESS_LOG = getModItem("appliedenergistics2", "item.ItemMultiMaterial", 1, 22);
-        final ItemStack AE2_PURE_CERTUS = getModItem("appliedenergistics2", "item.ItemMultiMaterial", 1, 10);
         final ItemStack AE2_QUARTZ_GLASS = getModItem("appliedenergistics2", "tile.BlockQuartzGlass", 1);
         final ItemStack AE2_LAMP_GLASS = getModItem("appliedenergistics2", "tile.BlockQuartzLamp", 1);
         final ItemStack AE2_CELL_HOUSING = getModItem("appliedenergistics2", "item.ItemMultiMaterial", 1, 39);
@@ -62,19 +55,44 @@ public class ScriptAE2FC implements IScriptLoader {
         final ItemStack CERTUS_PLATE = GT_OreDictUnificator.get(OrePrefixes.plate, Materials.NetherQuartz, 1L);
         final ItemStack LAPIS_SCREW = GT_OreDictUnificator.get(OrePrefixes.screw, Materials.Lapis, 1L);
         final ItemStack FLUID_CORE_1 = getModItem("dreamcraft", "item.EngineeringProcessorFluidDiamondCore", 1);
-        //registering cell
-        OreDictionary.registerOre("anyCertusCrystal", AE2_PURE_CERTUS);
-        for (ItemStack it : OreDictionary.getOres("crystalCertusQuartz"))
-            OreDictionary.registerOre("anyCertusCrystal", it);
+        final ItemStack CELL_1 = getModItem("ae2fc", "fluid_storage1", 1, 0);
+        final ItemStack CELL_4 = getModItem("ae2fc", "fluid_storage4", 1, 0);
+        final ItemStack CELL_16 = getModItem("ae2fc", "fluid_storage16", 1, 0);
+        final ItemStack CELL_64 = getModItem("ae2fc", "fluid_storage64", 1, 0);
+        final ItemStack CELL_256 = getModItem("ae2fc", "fluid_storage256", 1, 0);
+        final ItemStack CELL_1024 = getModItem("ae2fc", "fluid_storage1024", 1, 0);
+        final ItemStack CELL_4096 = getModItem("ae2fc", "fluid_storage4096", 1, 0);
+        final ItemStack COMPONENT_1 = getModItem("ae2fc", "fluid_part", 1, 0);
+        final ItemStack COMPONENT_4 = getModItem("ae2fc", "fluid_part", 1, 1);
+        final ItemStack COMPONENT_16 = getModItem("ae2fc", "fluid_part", 1, 2);
+        final ItemStack COMPONENT_64 = getModItem("ae2fc", "fluid_part", 1, 3);
+        final ItemStack COMPONENT_256 = getModItem("ae2fc", "fluid_part", 1, 4);
+        final ItemStack COMPONENT_1024 = getModItem("ae2fc", "fluid_part", 1, 5);
+        final ItemStack COMPONENT_4096 = getModItem("ae2fc", "fluid_part", 1, 6);
+        final ItemStack AE2FC_INTERFACE = getModItem("ae2fc", "fluid_interface", 1, 0);
+        final ItemStack AE2FC_DISCRETIZER = getModItem("ae2fc", "fluid_discretizer", 1, 0);
+        final ItemStack AE2FC_ENCODER = getModItem("ae2fc", "fluid_pattern_encoder", 1, 0);
+        final ItemStack AE2FC_DECODER = getModItem("ae2fc", "fluid_packet_decoder", 1, 0);
+        final ItemStack AE2FC_BUFFER = getModItem("ae2fc", "ingredient_buffer", 1, 0);
+        final ItemStack AE2FC_BUFFER_LARGE = getModItem("ae2fc", "large_ingredient_buffer", 1, 0);
+        final ItemStack AE2FC_TERMINAL = getModItem("ae2fc", "part_fluid_pattern_terminal", 1, 0);
+        final ItemStack AE2FC_TERMINAL_PRO = getModItem("ae2fc", "part_fluid_pattern_terminal_ex", 1, 0);
+        final ItemStack AE2FC_EXPORTBUS = getModItem("ae2fc", "part_fluid_export", 1, 0);
+        final ItemStack AE2FC_IMPORTBUS = getModItem("ae2fc", "part_fluid_import", 1, 0);
+        final ItemStack AE2FC_OCEDITOR = getModItem("ae2fc", "oc_pattern_editor", 1, 0);
 
-        ItemBasicFluidStorageCell[] cells = new ItemBasicFluidStorageCell[]{
-                CELL1K, CELL4K, CELL16K, CELL64K, CELL256K, CELL1024K, CELL4096K
+        ItemStack[] cells = new ItemStack[]{
+                CELL_1, CELL_4, CELL_16, CELL_64, CELL_256, CELL_1024, CELL_4096
+        };
+        ItemStack[] components = new ItemStack[]{
+                COMPONENT_1, COMPONENT_4, COMPONENT_16, COMPONENT_64, COMPONENT_256, COMPONENT_1024, COMPONENT_4096
         };
 
-        for (ItemBasicFluidStorageCell cell : cells) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(cell, "GDG", "DCD", "III", 'D', "dustRedstone", 'G', AE2_QUARTZ_GLASS, 'C', cell.getComponent(), 'I', "ingotIron"));
-            GameRegistry.addRecipe(new ShapelessOreRecipe(cell, AE2_CELL_HOUSING, cell.getComponent()));
+        for (int i = 0; i < cells.length; i++) {
+            GameRegistry.addShapelessRecipe(cells[i], new Object[]{AE2_CELL_HOUSING, components[i]});
+            GameRegistry.addRecipe(new ShapedOreRecipe(cells[i], "GDG", "DCD", "III", 'D', "dustRedstone", 'G', AE2_QUARTZ_GLASS, 'C', components[i], 'I', "ingotIron"));
         }
+
 
         //recursive components for those who want to do this weird stuff
         GameRegistry.addRecipe(new ShapedOreRecipe(CellType.Cell1kPart.stack(1), "DCD", "CEC", "DCD", 'D', "dyeBlue", 'C', "anyCertusCrystal", 'E', AE2_PROCESS_ENG));
@@ -408,61 +426,61 @@ public class ScriptAE2FC implements IScriptLoader {
         );
 
         //interface
-        addShapedRecipe(INTERFACE.stack(), new Object[]{
+        addShapedRecipe(AE2FC_INTERFACE, new Object[]{
                 IRON_PLATE, LAPIS_PLATE, IRON_PLATE,
                 FLUID_CORE_1, AE2_INTERFACE, FLUID_CORE_1,
                 IRON_PLATE, LAPIS_PLATE, IRON_PLATE});
         //Fluid discretizer
-        addShapedRecipe(DISCRETIZER.stack(), new Object[]{
+        addShapedRecipe(AE2FC_DISCRETIZER, new Object[]{
                 IRON_PLATE, BUCKET, IRON_PLATE,
                 AE2_STORAGE_BUS, ItemList.Electric_Pump_EV.get(1), AE2_STORAGE_BUS,
                 IRON_PLATE, AE2_PROCESS_ENG, IRON_PLATE});
         //Fluid encoder
-        addShapedRecipe(ENCODER.stack(), new Object[]{
+        addShapedRecipe(AE2FC_ENCODER, new Object[]{
                 LAPIS_PLATE, ItemList.Machine_HV_Assembler.get(1), LAPIS_PLATE,
                 IRON_PLATE, AE2_WORK_BENCH, IRON_PLATE,
                 IRON_PLATE, IRON_PLATE, IRON_PLATE});
-        //Fluid encoder
-        addShapedRecipe(DECODER.stack(), new Object[]{
+        //Fluid decoder
+        addShapedRecipe(AE2FC_DECODER, new Object[]{
                 IRON_PLATE, ItemList.Machine_HV_Disassembler.get(1), IRON_PLATE,
                 AE2_GLASS_CABLE, AE2_WORK_BENCH, AE2_GLASS_CABLE,
                 IRON_PLATE, IRON_PLATE, IRON_PLATE});
         //Ingredient Buffer
-        addShapedRecipe(BUFFER.stack(), new Object[]{
+        addShapedRecipe(AE2FC_BUFFER, new Object[]{
                 IRON_PLATE, FLUID_CORE_1, IRON_PLATE,
                 BUCKET, AE2_WORK_BENCH, BUCKET,
                 IRON_PLATE, AE2_PROCESS_CAL, IRON_PLATE});
         //Large Ingredient Buffer
-        addShapedRecipe(LARGE_BUFFER.stack(), new Object[]{
+        addShapedRecipe(AE2FC_BUFFER_LARGE, new Object[]{
                 NIOBIUM_PLATE, FLUID_CORE_1, NIOBIUM_PLATE,
-                ItemList.Super_Tank_LV.get(1), BUFFER.stack(), ItemList.Super_Chest_LV.get(1),
+                ItemList.Super_Tank_LV.get(1), AE2FC_BUFFER, ItemList.Super_Chest_LV.get(1),
                 NIOBIUM_PLATE, AE2_PROCESS_CAL, NIOBIUM_PLATE});
         //Fluid Terminal
-        addShapedRecipe(FLUID_TERMINAL.stack(), new Object[]{
+        addShapedRecipe(AE2FC_TERMINAL, new Object[]{
                 NIOBIUM_PLATE, FLUID_CORE_1, NIOBIUM_PLATE,
                 BUCKET, AE2_PATTERN_TERM, BUCKET,
                 NIOBIUM_PLATE, AE2_PROCESS_CAL, NIOBIUM_PLATE});
         //Fluid Processing Terminal
-        addShapedRecipe(FLUID_TERMINAL_EX.stack(), new Object[]{
-                LAPIS_PLATE, FLUID_TERMINAL.stack(), LAPIS_PLATE,
+        addShapedRecipe(AE2FC_TERMINAL_PRO, new Object[]{
+                LAPIS_PLATE, AE2FC_TERMINAL, LAPIS_PLATE,
                 ItemList.Conveyor_Module_LuV.get(1), AE2_CORE_ANN, ItemList.Conveyor_Module_LuV.get(1),
                 ItemList.Electric_Motor_LuV.get(1), ItemList.Robot_Arm_LuV.get(1), ItemList.Electric_Motor_LuV.get(1)});
 
-            //Fluid Export Bus
-            addShapedRecipe(FLUID_EXPORT_BUS.stack(), new Object[]{
-                    IRON_PLATE, LAPIS_PLATE, IRON_PLATE,
-                    LAPIS_SCREW, AE2_CORE_FOM, LAPIS_SCREW,
-                    CERTUS_PLATE, ItemList.Electric_Piston_LV.get(1), CERTUS_PLATE});
-            //Fluid Import Bus
-            addShapedRecipe(FLUID_IMPORT_BUS.stack(), new Object[]{
-                    IRON_PLATE, LAPIS_PLATE, IRON_PLATE,
-                    LAPIS_SCREW, AE2_CORE_ANN, LAPIS_SCREW,
-                    CERTUS_PLATE, ItemList.Electric_Piston_LV.get(1), CERTUS_PLATE});
+        //Fluid Export Bus
+        addShapedRecipe(AE2FC_EXPORTBUS, new Object[]{
+                IRON_PLATE, LAPIS_PLATE, IRON_PLATE,
+                LAPIS_SCREW, AE2_CORE_FOM, LAPIS_SCREW,
+                CERTUS_PLATE, ItemList.Electric_Piston_LV.get(1), CERTUS_PLATE});
+        //Fluid Import Bus
+        addShapedRecipe(AE2FC_IMPORTBUS, new Object[]{
+                IRON_PLATE, LAPIS_PLATE, IRON_PLATE,
+                LAPIS_SCREW, AE2_CORE_ANN, LAPIS_SCREW,
+                CERTUS_PLATE, ItemList.Electric_Piston_LV.get(1), CERTUS_PLATE});
         
         //OC Component
         if (Loader.isModLoaded("OpenComputers")) {
             ItemStack CHIP_T1 = getModItem("OpenComputers", "item", 1, 24);
-            GameRegistry.addRecipe(new ShapedOreRecipe(OC_EDITOR, "IMI", "CBC", "IPI", 'I', IRON_BAR, 'M', CHIP_T1, 'C', "oc:cable", 'B', BUCKET, 'P', AE2_BLANK_PATTERN));
+            GameRegistry.addRecipe(new ShapedOreRecipe(AE2FC_OCEDITOR, "IMI", "CBC", "IPI", 'I', IRON_BAR, 'M', CHIP_T1, 'C', "oc:cable", 'B', BUCKET, 'P', AE2_BLANK_PATTERN));
         }
 
     }
