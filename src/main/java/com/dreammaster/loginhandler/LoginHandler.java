81ec40dbfda21aa5fd8f3a125224ad38bbe51753
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2022-09-10 03:57:34 +0200

    minor text change

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -6,6 +6,7 @@ import static net.minecraft.util.EnumChatFormatting.BOLD;
 import static net.minecraft.util.EnumChatFormatting.DARK_GREEN;
 import static net.minecraft.util.EnumChatFormatting.GOLD;
 import static net.minecraft.util.EnumChatFormatting.GREEN;
+import static net.minecraft.util.EnumChatFormatting.STRIKETHROUGH;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
@@ -14,7 +15,7 @@ import net.minecraft.util.ChatComponentText;
 
 public class LoginHandler {
     private static final String[] LOGIN_TEXT = {
-        GOLD + "=====================================================",
+        GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------",
         BOLD + "Welcome to Gregtech: New Horizons " + GREEN + ModPackVersion,
         BLUE + "The Quest Book has a shortcut key, check your keybindings",
         BLUE + "to remove conflicts.",
@@ -22,7 +23,7 @@ public class LoginHandler {
         GREEN + "Please report bugs here:",
         GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/",
         BLUE + "Visit our discord at https://discord.gg/gtnh",
-        GOLD + "====================================================="
+        GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------"
     };
 
     @SuppressWarnings("unused")
