e560a385ed0e8b82157cfc737dd426048036d581
Runakai1 <48415331+Runakai1@users.noreply.github.com>
2022-09-13 19:11:45 +0200

    Dtpf bee part2 (#414)
    * Changed plasma discount
    
    * Apply new Discount to CSNT too
    
    * remove added dependencies

--- src/main/java/com/dreammaster/gthandler/GT_MachineRecipeLoader.java
+++ src/main/java/com/dreammaster/gthandler/GT_MachineRecipeLoader.java
@@ -7463,7 +7463,7 @@ public class GT_MachineRecipeLoader implements Runnable {
 
                 long tier_1_quantity = 144L * base_quantity;
                 // Bee Recipes
-                long fuel_quantity_bee_1 = 15442L;
+                long fuel_quantity_bee_1 = (long) (30883L * 0.85);
                 GT_Values.RA.addPlasmaForgeRecipe(
                         new ItemStack[] {
                             GT_ModHandler.getModItem("gregtech", "gt.comb", 1L, 117), GT_Utility.getIntegratedCircuit(1)
@@ -7482,7 +7482,7 @@ public class GT_MachineRecipeLoader implements Runnable {
                         awakened_heat);
 
                 long tier_2_bee_quantity = 144L * base_quantity * tier_up_multiplier;
-                long fuel_quantity_bee_2 = 6043L;
+                long fuel_quantity_bee_2 = (long) (12085L * 0.85);
                 GT_Values.RA.addPlasmaForgeRecipe(
                         new ItemStack[] {
                             GT_ModHandler.getModItem("gregtech", "gt.comb", 1L, 117), GT_Utility.getIntegratedCircuit(1)
@@ -7501,7 +7501,7 @@ public class GT_MachineRecipeLoader implements Runnable {
                         infinity_heat);
 
                 long tier_3_bee_quantity = 144L * base_quantity * tier_up_multiplier * tier_up_multiplier;
-                long fuel_quantity_bee_3 = 2608L;
+                long fuel_quantity_bee_3 = (long) (5215L * 0.85);
                 GT_Values.RA.addPlasmaForgeRecipe(
                         new ItemStack[] {
                             GT_ModHandler.getModItem("gregtech", "gt.comb", 1L, 117), GT_Utility.getIntegratedCircuit(1)
@@ -7521,7 +7521,7 @@ public class GT_MachineRecipeLoader implements Runnable {
 
                 long tier_4_bee_quantity =
                         144L * base_quantity * tier_up_multiplier * tier_up_multiplier * tier_up_multiplier;
-                long fuel_quantity_bee_4 = 1100L;
+                long fuel_quantity_bee_4 = (long) (2200L * 0.85);
                 GT_Values.RA.addPlasmaForgeRecipe(
                         new ItemStack[] {
                             GT_ModHandler.getModItem("gregtech", "gt.comb", 1L, 117), GT_Utility.getIntegratedCircuit(1)
@@ -7822,7 +7822,7 @@ public class GT_MachineRecipeLoader implements Runnable {
                 int base_time = 5_000;
 
                 // Bee comb catalyst recipes for infinity
-                long fuel_quantity_3 = 12_884;
+                long fuel_quantity_3 = (long) (25_767 * 0.85);
                 GT_Values.RA.addPlasmaForgeRecipe(
                         new ItemStack[] {
                             GT_ModHandler.getModItem("Avaritia", "Resource", 2L, 5),
@@ -7839,7 +7839,7 @@ public class GT_MachineRecipeLoader implements Runnable {
                         204_800_000,
                         eternal_heat);
 
-                long fuel_quantity_4 = 28_516;
+                long fuel_quantity_4 = (long) (57_031 * 0.85);
                 GT_Values.RA.addPlasmaForgeRecipe(
                         new ItemStack[] {
                             GT_ModHandler.getModItem("Avaritia", "Resource", 1L, 5),
@@ -7856,7 +7856,7 @@ public class GT_MachineRecipeLoader implements Runnable {
                         102_400_000,
                         hypogen_heat);
 
-                long fuel_quantity_special_bee = 57_031L / 128;
+                long fuel_quantity_special_bee = (long) ((57_031L / 64) * 0.75);
                 GT_Values.RA.addPlasmaForgeRecipe(
                         new ItemStack[] {
                             GT_ModHandler.getModItem("Avaritia", "Resource", 1L, 5),
