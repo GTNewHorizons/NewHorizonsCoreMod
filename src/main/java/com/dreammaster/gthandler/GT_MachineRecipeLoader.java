a9314a7caee2f810349110be3ca39500070374b1
POPlol333 <adampoplol@gmail.com>
2022-10-20 01:28:03 +0200

    remove unused import

--- src/main/java/com/dreammaster/gthandler/GT_MachineRecipeLoader.java
+++ src/main/java/com/dreammaster/gthandler/GT_MachineRecipeLoader.java
@@ -22,7 +22,6 @@ import java.lang.reflect.Field;
 import java.util.List;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
