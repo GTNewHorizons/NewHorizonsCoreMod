f42ce403d6e101fe56d758fb15e3736b7b476202
phineas <finny.masters@gmail.com>
2023-01-19 18:59:17 -0600

    Remove added test deps, fixed NPE crash, and finished the recipe.

--- src/main/java/com/dreammaster/scripts/ScriptThaumicEnergistics.java
+++ src/main/java/com/dreammaster/scripts/ScriptThaumicEnergistics.java
@@ -2,13 +2,11 @@ package com.dreammaster.scripts;
 
 import static gregtech.api.util.GT_ModHandler.getModItem;
 
+import gregtech.api.enums.ItemList;
 import net.minecraft.item.ItemStack;
 import thaumcraft.api.ThaumcraftApi;
 import thaumcraft.api.aspects.Aspect;
 import thaumcraft.api.aspects.AspectList;
-import thaumcraft.api.research.ResearchCategories;
-import thaumcraft.api.research.ResearchItem;
-import thaumcraft.api.research.ResearchPage;
 
 public class ScriptThaumicEnergistics implements IScriptLoader {
 
@@ -19,58 +17,40 @@ public class ScriptThaumicEnergistics implements IScriptLoader {
         scriptName.setLength(0);
         scriptName.append("ThaumicEnergistics");
         dependencies.clear();
-        dependencies.addAll(java.util.Arrays.asList(
-                "dreamcraft", "TaintedMagic", "gadomancy", "Thaumcraft", "thaumicenergistics", "extracells"));
+        dependencies.addAll(
+                java.util.Arrays.asList("dreamcraft", "TaintedMagic", "gadomancy", "Thaumcraft", "thaumicenergistics"));
     }
 
     @Override
     public void loadRecipes() {
-        ResearchItem research = new ResearchItem(
-                "ABCD",
-                "GOLEMANCY",
-                new AspectList()
-                        .add(Aspect.MAGIC, 1)
-                        .add(Aspect.HEAL, 1)
-                        .add(Aspect.PLANT, 1)
-                        .add(Aspect.EXCHANGE, 1),
-                -11,
-                11,
-                2,
-                getModItem("thaumicenergistics", "storage.essentia", 1, 4));
-        research.setPages(new ResearchPage("blah.translated"));
-
-        research.setParents("INFUSION");
-        ThaumcraftApi.addWarpToResearch("ABCD", 20);
-        ResearchCategories.addResearch(research);
-
         // Creates ItemStack for CEC craft input
         ItemStack[] CECInfusionItems = {
-            getModItem("dreamcraft", "item.PikoCircuit", 1),
-            getModItem("TaintedMagic", "ItemFocusDarkMatter", 1),
+            ItemList.Field_Generator_UIV.get(1),
+            getModItem("TaintedMagic", "ItemFocusEldritch", 1),
             getModItem("gadomancy", "BlockNodeManipulator", 1, 5),
             getModItem("gadomancy", "BlockEssentiaCompressor", 1),
-            getModItem("dreamcraft", "item.PikoCircuit", 1),
-            getModItem("TaintedMagic", "ItemFocusDarkMatter", 1),
+            ItemList.Field_Generator_UIV.get(1),
+            getModItem("TaintedMagic", "ItemFocusEldritch", 1),
             getModItem("gadomancy", "BlockNodeManipulator", 1, 5),
             getModItem("gadomancy", "BlockEssentiaCompressor", 1),
-            getModItem("dreamcraft", "item.PikoCircuit", 1),
-            getModItem("TaintedMagic", "ItemFocusDarkMatter", 1),
+            ItemList.Field_Generator_UIV.get(1),
+            getModItem("TaintedMagic", "ItemFocusEldritch", 1),
             getModItem("gadomancy", "BlockNodeManipulator", 1, 5),
             getModItem("gadomancy", "BlockEssentiaCompressor", 1),
-            getModItem("dreamcraft", "item.PikoCircuit", 1),
-            getModItem("TaintedMagic", "ItemFocusDarkMatter", 1),
+            ItemList.Field_Generator_UIV.get(1),
+            getModItem("TaintedMagic", "ItemFocusEldritch", 1),
             getModItem("gadomancy", "BlockNodeManipulator", 1, 5),
             getModItem("gadomancy", "BlockEssentiaCompressor", 1)
         };
 
         // ItemStacks for in/out
-        ItemStack SingularityDrive = getModItem("extracells", "storage.physical.advanced.singularity", 1);
+        ItemStack SingularityDrive = getModItem("eternalsingularity", "eternal_singularity", 1);
         ItemStack CEC = getModItem("thaumicenergistics", "storage.essentia", 1, 4);
 
         ThaumcraftApi.addInfusionCraftingRecipe(
-                "ABCD",
+                "thaumicenergistics.TESTORAGE",
                 CEC,
-                0,
+                10,
                 new AspectList()
                         .add(Aspect.AIR, 2000)
                         .add(Aspect.FIRE, 2000)
