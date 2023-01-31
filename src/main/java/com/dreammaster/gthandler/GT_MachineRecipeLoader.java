afe2f8da361aef6e8eadc5979ca5a34f3b3de8e5
boubou19 <miisterunknown@gmail.com>
2023-01-31 12:05:28 +0100

    moved fluid canner recipes

--- src/main/java/com/dreammaster/gthandler/GT_MachineRecipeLoader.java
+++ src/main/java/com/dreammaster/gthandler/GT_MachineRecipeLoader.java
@@ -39,80 +39,11 @@ public class GT_MachineRecipeLoader implements Runnable {
         new CompressorRecipes().run();
         new DistilleryRecipes().run();
         new DTPFRecipes().run();
+        new FluidCannerRecipes().run();
         new FluidSolidifierRecipes().run();
         new ImplosionCompressorRecipes().run();
         new MixerRecipes().run();
 
-        // NAK and Helium Cooling Cells
-        GT_Values.RA.addFluidCannerRecipe(
-                CustomItemList.TenKCell.get(1L),
-                ItemList.Reactor_Coolant_NaK_1.get(1L),
-                new FluidStack(FluidRegistry.getFluid("sodiumpotassium"), 1000),
-                GT_Values.NF);
-        GT_Values.RA.addFluidCannerRecipe(
-                CustomItemList.ThirtyKCell.get(1L),
-                ItemList.Reactor_Coolant_NaK_3.get(1L),
-                new FluidStack(FluidRegistry.getFluid("sodiumpotassium"), 3000),
-                GT_Values.NF);
-        GT_Values.RA.addFluidCannerRecipe(
-                CustomItemList.SixtyKCell.get(1L),
-                ItemList.Reactor_Coolant_NaK_6.get(1L),
-                new FluidStack(FluidRegistry.getFluid("sodiumpotassium"), 6000),
-                GT_Values.NF);
-        if ((Loader.isModLoaded("GalaxySpace")) && (Loader.isModLoaded("bartworks"))) {
-            GT_Values.RA.addFluidCannerRecipe(
-                    CustomItemList.TenKCell.get(1L),
-                    ItemList.Reactor_Coolant_He_1.get(1L),
-                    new FluidStack(FluidRegistry.getFluid("liquid helium"), 1000),
-                    GT_Values.NF);
-            GT_Values.RA.addFluidCannerRecipe(
-                    CustomItemList.ThirtyKCell.get(1L),
-                    ItemList.Reactor_Coolant_He_3.get(1L),
-                    new FluidStack(FluidRegistry.getFluid("liquid helium"), 3000),
-                    GT_Values.NF);
-            GT_Values.RA.addFluidCannerRecipe(
-                    CustomItemList.SixtyKCell.get(1L),
-                    ItemList.Reactor_Coolant_He_6.get(1L),
-                    new FluidStack(FluidRegistry.getFluid("liquid helium"), 6000),
-                    GT_Values.NF);
-        } else {
-            GT_Values.RA.addFluidCannerRecipe(
-                    CustomItemList.TenKCell.get(1L),
-                    ItemList.Reactor_Coolant_He_1.get(1L),
-                    Materials.Helium.getGas(1000L),
-                    GT_Values.NF);
-            GT_Values.RA.addFluidCannerRecipe(
-                    CustomItemList.TenKCell.get(1L),
-                    ItemList.Reactor_Coolant_He_3.get(1L),
-                    Materials.Helium.getGas(3000L),
-                    GT_Values.NF);
-            GT_Values.RA.addFluidCannerRecipe(
-                    CustomItemList.TenKCell.get(1L),
-                    ItemList.Reactor_Coolant_He_6.get(1L),
-                    Materials.Helium.getGas(6000L),
-                    GT_Values.NF);
-        }
-        // Space Cooling Cells
-        GT_Values.RA.addFluidCannerRecipe(
-                CustomItemList.Empty180SpCell.get(1L),
-                ItemList.Reactor_Coolant_Sp_1.get(1L),
-                Materials.SuperCoolant.getFluid(1000L),
-                GT_Values.NF);
-        GT_Values.RA.addFluidCannerRecipe(
-                CustomItemList.Empty360SpCell.get(1L),
-                ItemList.Reactor_Coolant_Sp_2.get(1L),
-                Materials.SuperCoolant.getFluid(2000L),
-                GT_Values.NF);
-        GT_Values.RA.addFluidCannerRecipe(
-                CustomItemList.Empty540SpCell.get(1L),
-                ItemList.Reactor_Coolant_Sp_3.get(1L),
-                Materials.SuperCoolant.getFluid(3000L),
-                GT_Values.NF);
-        GT_Values.RA.addFluidCannerRecipe(
-                CustomItemList.Empty1080SpCell.get(1L),
-                ItemList.Reactor_Coolant_Sp_6.get(1L),
-                Materials.SuperCoolant.getFluid(6000L),
-                GT_Values.NF);
 
         GT_Values.RA.addAlloySmelterRecipe(
                 CustomItemList.MicaBasedSheet.get(4L),
