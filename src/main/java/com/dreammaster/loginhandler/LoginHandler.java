c61a58f4f0b7acd09a70325077e26418f145e902
Martin Robertz <dream-master@gmx.net>
2023-02-02 22:28:07 +0100

    update bs remove ae2 warning. We want bug report.

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -15,10 +15,9 @@ public class LoginHandler {
     private static final String[] LOGIN_TEXT = {
             GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------",
             BOLD + "Welcome to Gregtech: New Horizons " + GREEN + ModPackVersion,
-            RED + "We are aware that in this version, ", RED + "pattern substitution can behave weirdly.",
-            RED + "Please don't report it, thanks.", BLUE + "The Quest Book has a shortcut key, check your keybindings",
-            BLUE + "to remove conflicts.", DARK_GREEN + "GTNH WIKI link https://gtnh.miraheze.org/wiki/",
-            GREEN + "Please report bugs here:", GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/",
+            BLUE + "The Quest Book has a shortcut key, check your keybindings", BLUE + "to remove conflicts.",
+            DARK_GREEN + "GTNH WIKI link https://gtnh.miraheze.org/wiki/", GREEN + "Please report bugs here:",
+            GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/",
             BLUE + "Visit our discord at https://discord.gg/gtnh",
             GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------" };
 
