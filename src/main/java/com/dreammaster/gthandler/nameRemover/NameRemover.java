c348cf1db2e5e9b9266c9b4188328e00779c0b7d
BritishCynic <TwoTailedFox@Gmail.com>
2023-02-02 18:55:40 +0000

    Update NameRemover.java (#504)
    * Update NameRemover.java
    
    This adds new functionality to the Name Remover
    
    * Can now remove dyes from armor
    * Can now remove the RepairCost NBT tag
    * Can now remove the "color" NBT tag used when Railcraft tank blocks are removed from the world
    
    * SpotlessApply
    
    * Update NameRemover.java
    
    Update logic, add ability to remove spray colour from GT items and separated out ability to remove dye into its own check.
    
    SA also run
    
    ---------
    
    Co-authored-by: Martin Robertz <dream-master@gmx.net>

--- src/main/java/com/dreammaster/gthandler/nameRemover/NameRemover.java
+++ src/main/java/com/dreammaster/gthandler/nameRemover/NameRemover.java
@@ -66,7 +66,8 @@ public class NameRemover extends GT_MetaTileEntity_BasicMachine {
 
         ItemStack output = getInputAt(0).copy();
         NBTTagCompound nbt = output.getTagCompound();
-        boolean removeName = false, removeDisassembly = false;
+        boolean removeName = false, removeDye = false, removeDisassembly = false, removeColor = false,
+                removeRepair = false, removeSpray = false;
 
         if (nbt != null) {
             ItemStack circuit = getInputAt(1);
@@ -81,11 +82,26 @@ public class NameRemover extends GT_MetaTileEntity_BasicMachine {
                 case 2:
                     removeDisassembly = true;
                     break;
+                case 3:
+                    removeColor = true;
+                    break;
+                case 4:
+                    removeRepair = true;
+                    break;
+                case 5:
+                    removeDye = true;
+                    break;
+                case 6:
+                    removeSpray = true;
+                    break;
                 default:
                     removeName = true;
                     removeDisassembly = true;
+                    removeColor = true;
+                    removeRepair = true;
+                    removeDye = true;
+                    removeSpray = true;
             }
-
             if (removeName && nbt.hasKey("display")) {
                 nbt.getCompoundTag("display").removeTag("Name");
                 if (nbt.getCompoundTag("display").hasNoTags()) {
@@ -94,6 +110,21 @@ public class NameRemover extends GT_MetaTileEntity_BasicMachine {
             }
             if (removeDisassembly && nbt.hasKey("GT.CraftingComponents")) {
                 nbt.removeTag("GT.CraftingComponents");
+            }
+            if (removeColor && nbt.hasKey("color")) {
+                nbt.removeTag("color");
+            }
+            if (removeRepair && nbt.hasKey("RepairCost")) {
+                nbt.removeTag("RepairCost");
+            }
+            if (removeDye && nbt.hasKey("display")) {
+                nbt.getCompoundTag("display").removeTag("color");
+                if (nbt.getCompoundTag("display").hasNoTags()) {
+                    nbt.removeTag("display");
+                }
+            }
+            if (removeSpray && nbt.hasKey("mColor")) {
+                nbt.removeTag("mColor");
             }
             if (nbt.hasNoTags()) {
                 output.setTagCompound(null);
@@ -120,7 +151,11 @@ public class NameRemover extends GT_MetaTileEntity_BasicMachine {
         description.add(UNDERLINE + "Second Slot" + RESET);
         description.add("One of the following circuits:");
         description.add(BOLD + "Circuit 1:" + RESET + "  Attempt to fix broken ores by removing the Display Name tag");
-        description.add(BOLD + "Circuit 2:" + RESET + "  Remove disassembly tags");
+        description.add(BOLD + "Circuit 2:" + RESET + "  Remove GT Disassembler tags");
+        description.add(BOLD + "Circuit 3:" + RESET + "  Remove Railcraft stacking tag");
+        description.add(BOLD + "Circuit 4:" + RESET + "  Remove Anvil repair tag");
+        description.add(BOLD + "Circuit 5:" + RESET + "  Remove Dye from Leather armor");
+        description.add(BOLD + "Circuit 6:" + RESET + "  Remove Spray color from GT items");
         description.add(" ");
         description.add(BOLD + "No Circuit:" + RESET + " Remove all of the above");
 
