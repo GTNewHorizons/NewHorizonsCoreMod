b6c665f4c84543f141589f2f2db91f3bcade2b95
Martin Robertz <dream-master@gmx.net>
2023-02-17 21:47:21 +0100

    bump version

--- src/main/java/com/dreammaster/config/CoreModConfig.java
+++ src/main/java/com/dreammaster/config/CoreModConfig.java
@@ -53,7 +53,7 @@ public class CoreModConfig extends ConfigManager {
     @Override
     protected void PreInit() {
         ModLoginMessage_Enabled = true;
-        ModPackVersion = "2.2.9";
+        ModPackVersion = "2.2.10";
         ModDebugVersionDisplay_Enabled = true;
         ModHazardousItems_Enabled = false;
         ModCustomToolTips_Enabled = false;
