5a345e73a156487311af181a12aa9e92cdfa7331
Martin Robertz <dream-master@gmx.net>
2023-03-01 15:22:50 +0100

    correct version

--- src/main/java/com/dreammaster/config/CoreModConfig.java
+++ src/main/java/com/dreammaster/config/CoreModConfig.java
@@ -53,7 +53,7 @@ public class CoreModConfig extends ConfigManager {
     @Override
     protected void PreInit() {
         ModLoginMessage_Enabled = true;
-        ModPackVersion = "2.2.3";
+        ModPackVersion = "2.3.0";
         ModDebugVersionDisplay_Enabled = true;
         ModHazardousItems_Enabled = false;
         ModCustomToolTips_Enabled = false;
