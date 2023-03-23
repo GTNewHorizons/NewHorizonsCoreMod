edb0205adac61f2668501506e3b0b730a70e9e5f
Martin Robertz <dream-master@gmx.net>
2023-03-23 13:13:37 +0100

    Ender io recipes (#544)
    * update bs+sa
    
    * add Me conduit recipes to code
    
    * remove me conduits crafting recipes
    
    * change Ultra dense me conduit recipe use molten silicone
    
    * change gt fluids to ender io fluids
    add Circuit to conduits
    change ratio 16->4
    
    * spotlessApply (#543)
    
    Co-authored-by: GitHub GTNH Actions <>
    
    * change ratio to 1:1
    
    * sa
    
    * derp
    
    * add control circuit
    
    * sa
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/main/java/com/dreammaster/scripts/ScriptEnderIO.java
+++ src/main/java/com/dreammaster/scripts/ScriptEnderIO.java
@@ -13,6 +13,7 @@ import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.util.GT_OreDictUnificator;
+import gregtech.api.util.GT_Utility;
 
 public class ScriptEnderIO implements IScriptLoader {
 
@@ -34,6 +35,7 @@ public class ScriptEnderIO implements IScriptLoader {
         ItemStack staffOfTravelling = getModItem("EnderIO", "itemTravelStaff", 1, wildcard);
         ItemStack endestPearl = getModItem("Avaritia", "Endest_Pearl", 1);
         ItemStack fieldGeneratorZPM = ItemList.Field_Generator_ZPM.get(1);
+        ItemStack circuit2 = GT_Utility.getIntegratedCircuit(2);
 
         ExtremeCraftingManager.getInstance().addExtremeShapedOreRecipe(
                 teleportStaff,
@@ -61,7 +63,7 @@ public class ScriptEnderIO implements IScriptLoader {
         // ME Conduit
         GT_Values.RA.addAssemblerRecipe(
                 new ItemStack[] { getModItem("appliedenergistics2", "item.ItemMultiPart", 4, 16),
-                        GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Good, 1L) },
+                        GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Good, 1L), circuit2 },
                 Materials.ConductiveIron.getMolten(144L),
                 getModItem("EnderIO", "itemMEConduit", 4),
                 200,
@@ -69,7 +71,7 @@ public class ScriptEnderIO implements IScriptLoader {
         // ME Dense Conduit
         GT_Values.RA.addAssemblerRecipe(
                 new ItemStack[] { getModItem("EnderIO", "itemMEConduit", 16),
-                        GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Advanced, 1L) },
+                        GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Advanced, 1L), circuit2 },
                 Materials.EnergeticAlloy.getMolten(144L),
                 getModItem("EnderIO", "itemMEConduit", 4, 1),
                 200,
@@ -77,7 +79,7 @@ public class ScriptEnderIO implements IScriptLoader {
         // ME Ultra Dense Conduit
         GT_Values.RA.addAssemblerRecipe(
                 new ItemStack[] { getModItem("EnderIO", "itemMEConduit", 16, 1),
-                        GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Data, 1L) },
+                        GT_OreDictUnificator.get(OrePrefixes.circuit, Materials.Data, 1L), circuit2 },
                 Materials.VibrantAlloy.getMolten(144L),
                 getModItem("EnderIO", "itemMEConduit", 4, 2),
                 200,
