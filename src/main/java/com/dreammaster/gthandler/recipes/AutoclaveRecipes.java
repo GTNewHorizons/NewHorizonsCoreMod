21aff20c88b947c9c667a593447cf9480b4f203c
GDCloud <gdcloudstrike@gmail.com>
2023-04-04 22:30:42 +0200

    bio wafer

--- src/main/java/com/dreammaster/gthandler/recipes/AutoclaveRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/AutoclaveRecipes.java
@@ -1,9 +1,6 @@
 package com.dreammaster.gthandler.recipes;
 
-import static gregtech.api.enums.Mods.EnderZoo;
-import static gregtech.api.enums.Mods.GalacticraftMars;
-import static gregtech.api.enums.Mods.Gendustry;
-import static gregtech.api.enums.Mods.SGCraft;
+import static gregtech.api.enums.Mods.*;
 
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
@@ -11,13 +8,11 @@ import net.minecraftforge.fluids.FluidRegistry;
 
 import com.dreammaster.gthandler.CustomItemList;
 
-import gregtech.api.enums.GT_Values;
-import gregtech.api.enums.ItemList;
-import gregtech.api.enums.Materials;
-import gregtech.api.enums.OrePrefixes;
+import gregtech.api.enums.*;
 import gregtech.api.util.GT_ModHandler;
 import gregtech.api.util.GT_OreDictUnificator;
 import gregtech.api.util.GT_Utility;
+import gtPlusPlus.core.material.ELEMENT;
 
 public class AutoclaveRecipes implements Runnable {
 
@@ -143,6 +138,17 @@ public class AutoclaveRecipes implements Runnable {
                     8000,
                     12000,
                     480);
+        }
+        if (GTPlusPlus.isModLoaded()) {
+            GT_Values.RA.addAutoclaveRecipe(
+                    ItemList.Circuit_Silicon_Wafer6.get(1L),
+                    ELEMENT.STANDALONE.HYPOGEN.getDust(1),
+                    Materials.BioMediumSterilized.getFluid(8_000L),
+                    ItemList.Circuit_Wafer_Bioware.get(1L),
+                    10000,
+                    60 * 20,
+                    (int) TierEU.RECIPE_UHV,
+                    false);
         }
     }
 
