76931d5f14a83e7bbd2a35e6787f20739c61b271
kuba6000 <kuba.123123.6000@gmail.com>
2023-04-30 19:47:08 +0200

    Tinkers-Defence.zs

--- src/main/java/com/dreammaster/scripts/ScriptTinkersDefence.java
+++ src/main/java/com/dreammaster/scripts/ScriptTinkersDefence.java
@@ -1,11 +1,13 @@
 package com.dreammaster.scripts;
 
 import static gregtech.api.util.GT_ModHandler.getModItem;
+import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sCompressorRecipes;
 import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sExtruderRecipes;
 
 import java.util.Arrays;
 import java.util.List;
 
+import com.dreammaster.tinkersConstruct.TConstructHelper;
 import gregtech.api.enums.GT_Values;
 
 public class ScriptTinkersDefence implements IScriptLoader {
@@ -22,6 +24,18 @@ public class ScriptTinkersDefence implements IScriptLoader {
 
     @Override
     public void loadRecipes() {
+        TConstructHelper.removeTableRecipe(getModItem("TConstruct", "arrowhead", 1, 201, missing));
+        TConstructHelper.removeTableRecipe(getModItem("TConstruct", "arrowhead", 1, 203, missing));
+        TConstructHelper.removeTableRecipe(getModItem("TConstruct", "arrowhead", 1, 202, missing));
+        GT_Values.RA.stdBuilder().itemInputs(getModItem("tinkersdefense", "AeonSteelIngot", 9, 0, missing))
+                .itemOutputs(getModItem("tinkersdefense", "AeonSteelBlock", 1, 0, missing)).noFluidInputs()
+                .noFluidOutputs().duration(300).eut(2).addTo(sCompressorRecipes);
+        GT_Values.RA.stdBuilder().itemInputs(getModItem("tinkersdefense", "QueensGoldIngot", 9, 0, missing))
+                .itemOutputs(getModItem("tinkersdefense", "QueensGoldBlock", 1, 0, missing)).noFluidInputs()
+                .noFluidOutputs().duration(300).eut(2).addTo(sCompressorRecipes);
+        GT_Values.RA.stdBuilder().itemInputs(getModItem("tinkersdefense", "DogbeariumIngot", 9, 0, missing))
+                .itemOutputs(getModItem("tinkersdefense", "DogbeariumBlock", 1, 0, missing)).noFluidInputs()
+                .noFluidOutputs().duration(300).eut(2).addTo(sCompressorRecipes);
         GT_Values.RA.stdBuilder()
                 .itemInputs(
                         getModItem("tinkersdefense", "AeonSteelIngot", 1, 0, missing),
