9b815a023b168e1194c3db5e05f2c4095b8be4b6
kuba6000 <kuba.123123.6000@gmail.com>
2023-04-30 22:16:44 +0200

    Update BloodMagicHelper.java

--- src/main/java/com/dreammaster/bloodmagic/BloodMagicHelper.java
+++ src/main/java/com/dreammaster/bloodmagic/BloodMagicHelper.java
@@ -3,11 +3,21 @@ package com.dreammaster.bloodmagic;
 import net.minecraft.item.ItemStack;
 
 import WayofTime.alchemicalWizardry.api.alchemy.AlchemyRecipeRegistry;
+import WayofTime.alchemicalWizardry.api.altarRecipeRegistry.AltarRecipeRegistry;
+import WayofTime.alchemicalWizardry.api.bindingRegistry.BindingRegistry;
 import gregtech.api.util.GT_Utility;
 
 public class BloodMagicHelper {
 
     public static void removeAlchemyRecipe(ItemStack output) {
         AlchemyRecipeRegistry.recipes.removeIf(r -> GT_Utility.areStacksEqual(r.getResult(), output, true));
+    }
+
+    public static void removeBindingRecipe(ItemStack output) {
+        BindingRegistry.bindingRecipes.removeIf(r -> GT_Utility.areStacksEqual(r.getResult(), output, true));
+    }
+
+    public static void removeAltarRecipe(ItemStack output) {
+        AltarRecipeRegistry.altarRecipes.removeIf(r -> GT_Utility.areStacksEqual(r.getResult(), output, true));
     }
 }
