d0df45a3fd6fd12e2c552fedec472f09ff1d37e9
kuba6000 <kuba.123123.6000@gmail.com>
2023-05-06 21:03:59 +0200

    Add JavaDoc to createItemStack

--- src/main/java/com/dreammaster/scripts/IScriptLoader.java
+++ src/main/java/com/dreammaster/scripts/IScriptLoader.java
@@ -164,6 +164,20 @@ public interface IScriptLoader {
         return new ItemStack(GT_MetaGenerated_Item_01.INSTANCE, 1, meta);
     }
 
+    /**
+     * Helper function to get a new ItemStack with specified damage (meta) value and NBT tags. Calls
+     * {@link gregtech.api.util.GT_ModHandler#getModItem(java.lang.String, java.lang.String, long, int)} internally
+     *
+     * @param aModID       Mod ID of an the item
+     * @param aItem        Item registry name
+     * @param aAmount      Amount to get
+     * @param aMeta        the meta id of the item to look at.
+     * @param aNBTString   NBT data that the created stack should get, in format that {@link NBTTagCompound#toString()}
+     *                     returns. Can be checked in-game with `/iih` command
+     * @param aReplacement Replacement stack to return if the item is not found. NBT data is not applied to it.
+     * @return Created ItemStack or replacement stack
+     * @throws RuntimeException if the NBT string parsing fails
+     */
     default ItemStack createItemStack(String aModID, String aItem, long aAmount, int aMeta, String aNBTString,
             ItemStack aReplacement) {
         ItemStack s = getModItem(aModID, aItem, aAmount, aMeta);
