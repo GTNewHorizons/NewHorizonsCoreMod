791aeb4bf60e697c0bb2faf09fd72023d93c535a
boubou19 <miisterunknown@gmail.com>
2023-05-08 19:20:15 +0200

    Revert "distillation recipes"
    This reverts commit 76082e3a0b0a6a09696a76400103a292daacd114.

--- src/main/java/com/dreammaster/gthandler/recipes/DistillationRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/DistillationRecipes.java
@@ -1,18 +1,14 @@
 package com.dreammaster.gthandler.recipes;
 
 import static gregtech.api.enums.Mods.BartWorks;
-import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sDistillationRecipes;
-import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sDistilleryRecipes;
-import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
-import static gregtech.api.util.GT_RecipeBuilder.TICKS;
 
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
-import gregtech.api.enums.TierEU;
 import gregtech.api.util.GT_OreDictUnificator;
 import gregtech.api.util.GT_Utility;
 
@@ -25,66 +21,76 @@ public class DistillationRecipes implements Runnable {
     }
 
     public static void distillationTowerRecipes() {
-        GT_Values.RA.stdBuilder().itemInputs(GT_Utility.getIntegratedCircuit(1))
-                .itemOutputs(GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1))
-                .fluidInputs(new FluidStack(FluidRegistry.getFluid("pollution"), 1000))
-                .fluidOutputs(
-                        Materials.SulfuricAcid.getFluid(150),
-                        Materials.NitrogenDioxide.getGas(150),
-                        Materials.Methane.getGas(150),
-                        Materials.Mercury.getFluid(10))
-                .duration(30 * SECONDS).eut(TierEU.RECIPE_HV).addTo(sDistillationRecipes);
-
-        GT_Values.RA.stdBuilder().itemInputs(GT_Utility.getIntegratedCircuit(2))
-                .itemOutputs(GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 10))
-                .fluidInputs(new FluidStack(FluidRegistry.getFluid("pollution"), 10000))
-                .fluidOutputs(
-                        Materials.SulfuricAcid.getFluid(3000),
-                        Materials.NitrogenDioxide.getGas(3000),
-                        Materials.Methane.getGas(3000),
-                        Materials.Mercury.getFluid(200))
-                .duration(30 * SECONDS).eut(TierEU.RECIPE_EV).addTo(sDistillationRecipes);
+        GT_Values.RA.addDistillationTowerRecipe(
+                new FluidStack(FluidRegistry.getFluid("pollution"), 1000),
+                new ItemStack[] { GT_Utility.getIntegratedCircuit(1) },
+                new FluidStack[] { Materials.SulfuricAcid.getFluid(150L), Materials.NitrogenDioxide.getGas(150L),
+                        Materials.Methane.getGas(150L), Materials.Mercury.getFluid(10L) },
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 1L),
+                600,
+                480);
+        GT_Values.RA.addDistillationTowerRecipe(
+                new FluidStack(FluidRegistry.getFluid("pollution"), 10000),
+                new ItemStack[] { GT_Utility.getIntegratedCircuit(2) },
+                new FluidStack[] { Materials.SulfuricAcid.getFluid(3000L), Materials.NitrogenDioxide.getGas(3000L),
+                        Materials.Methane.getGas(3000L), Materials.Mercury.getFluid(200L) },
+                GT_OreDictUnificator.get(OrePrefixes.dust, Materials.Ash, 10L),
+                600,
+                1920);
 
         if (BartWorks.isModLoaded()) {
+            GT_Values.RA.addDistillationTowerRecipe(
+                    Materials.Grade2PurifiedWater.getFluid(1000L),
+                    new FluidStack[] { Materials.Grade3PurifiedWater.getFluid(900L),
+                            Materials.Grade2PurifiedWater.getFluid(50L), Materials.Grade1PurifiedWater.getFluid(100L),
+                            Materials.Oil.getFluid(50L), Materials.Ammonia.getGas(100L) },
+                    Materials.Calcite.getDust(1),
+                    500 * 10,
+                    122_880);
 
-            GT_Values.RA.stdBuilder().noItemInputs().noItemOutputs()
-                    .fluidInputs(Materials.Grade2PurifiedWater.getFluid(1000))
-                    .fluidOutputs(
-                            Materials.Grade3PurifiedWater.getFluid(900),
-                            Materials.Grade2PurifiedWater.getFluid(50),
-                            Materials.Grade1PurifiedWater.getFluid(100),
-                            Materials.Oil.getFluid(50),
-                            Materials.Ammonia.getGas(100))
-                    .duration(250 * SECONDS).eut(TierEU.RECIPE_ZPM).addTo(sDistillationRecipes);
-
-            GT_Values.RA.stdBuilder().noItemInputs().noItemOutputs()
-                    .fluidInputs(Materials.Grade6PurifiedWater.getFluid(1000))
-                    .fluidOutputs(
-                            Materials.Grade7PurifiedWater.getFluid(900),
-                            Materials.Grade6PurifiedWater.getFluid(50),
-                            Materials.Grade5PurifiedWater.getFluid(100),
-                            Materials.Ethanol.getFluid(50))
-                    .duration(250 * SECONDS).eut(TierEU.RECIPE_UHV).addTo(sDistillationRecipes);
+            GT_Values.RA.addDistillationTowerRecipe(
+                    Materials.Grade6PurifiedWater.getFluid(1000L),
+                    new FluidStack[] { Materials.Grade7PurifiedWater.getFluid(900L),
+                            Materials.Grade6PurifiedWater.getFluid(50L), Materials.Grade5PurifiedWater.getFluid(100L),
+                            Materials.Ethanol.getFluid(50L) },
+                    null,
+                    500 * 10,
+                    1_966_080);
         }
     }
 
     public static void distilleryRecipes() {
         // Sodium Potassium
-        GT_Values.RA.stdBuilder().itemInputs(Materials.RockSalt.getDust(1)).noItemOutputs()
-                .fluidInputs(Materials.Sodium.getFluid(1000))
-                .fluidOutputs(new FluidStack(FluidRegistry.getFluid("sodiumpotassium"), 1000)).duration(20 * SECONDS)
-                .eut(TierEU.RECIPE_LV).addTo(sDistilleryRecipes);
+        GT_Values.RA.addDistilleryRecipe(
+                Materials.RockSalt.getDust(1),
+                Materials.Sodium.getFluid(1000),
+                new FluidStack(FluidRegistry.getFluid("sodiumpotassium"), 1000),
+                400,
+                30,
+                false);
 
-        GT_Values.RA.stdBuilder().itemInputs(GT_Utility.getIntegratedCircuit(1)).noItemOutputs()
-                .fluidInputs(Materials.Biomass.getFluid(40)).fluidOutputs(Materials.Ethanol.getFluid(20))
-                .duration(16 * TICKS).eut(24).addTo(sDistilleryRecipes);
+        GT_Values.RA.addDistilleryRecipe(
+                GT_Utility.getIntegratedCircuit(1),
+                Materials.Biomass.getFluid(40L),
+                Materials.Ethanol.getFluid(20L),
+                16,
+                24,
+                false);
 
-        GT_Values.RA.stdBuilder().itemInputs(GT_Utility.getIntegratedCircuit(1)).noItemOutputs()
-                .fluidInputs(Materials.Milk.getFluid(1000)).fluidOutputs(FluidRegistry.getFluidStack("fluidmilk", 1000))
-                .duration(5 * SECONDS).eut(2).addTo(sDistilleryRecipes);
+        GT_Values.RA.addDistilleryRecipe(
+                GT_Utility.getIntegratedCircuit(1),
+                Materials.Milk.getFluid(1000L),
+                FluidRegistry.getFluidStack("fluidmilk", 1000),
+                100,
+                2,
+                false);
 
-        GT_Values.RA.stdBuilder().itemInputs(GT_Utility.getIntegratedCircuit(1)).noItemOutputs()
-                .fluidInputs(FluidRegistry.getFluidStack("fluidmilk", 1000)).fluidOutputs(Materials.Milk.getFluid(1000))
-                .duration(5 * SECONDS).eut(2).addTo(sDistilleryRecipes);
+        GT_Values.RA.addDistilleryRecipe(
+                GT_Utility.getIntegratedCircuit(1),
+                FluidRegistry.getFluidStack("fluidmilk", 1000),
+                Materials.Milk.getFluid(1000L),
+                100,
+                2,
+                false);
     }
 }
