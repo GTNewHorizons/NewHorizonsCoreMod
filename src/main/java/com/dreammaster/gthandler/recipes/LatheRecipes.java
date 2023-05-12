460000d7b6431c2d3af82bd14618de3218b30c79
chochem <40274384+chochem@users.noreply.github.com>
2023-05-12 20:53:44 +0100

    Remove broken duplicate recipes with forestry slabs and other duplicate slab recipes (#597)
    * forestry slab fix
    
    * remove (its in GT!)
    
    * remove more duplicate recipes

--- src/main/java/com/dreammaster/gthandler/recipes/LatheRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/LatheRecipes.java
@@ -1,15 +1,9 @@
 package com.dreammaster.gthandler.recipes;
 
-import static gregtech.api.enums.Mods.Forestry;
 import static gregtech.api.enums.Mods.GTPlusPlus;
 import static gregtech.api.util.GT_Recipe.GT_Recipe_Map.sLatheRecipes;
 import static gregtech.api.util.GT_RecipeBuilder.MINUTES;
 import static gregtech.api.util.GT_RecipeBuilder.SECONDS;
-import static gregtech.api.util.GT_RecipeBuilder.TICKS;
-
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
 
 import com.dreammaster.gthandler.CustomItemList;
 import com.dreammaster.gthandler.GT_CoreModSupport;
@@ -39,67 +33,5 @@ public class LatheRecipes implements Runnable {
                         CustomItemList.ReinforcedGlassLense.get(1L),
                         GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Glass, 1L))
                 .noFluidInputs().noFluidOutputs().duration(20 * SECONDS).eut(16).addTo(sLatheRecipes);
-
-        GT_Values.RA.stdBuilder().itemInputs(new ItemStack(Blocks.wooden_slab, 1, 0))
-                .itemOutputs(
-                        new ItemStack(Items.bowl, 1),
-                        GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8).addTo(sLatheRecipes);
-
-        GT_Values.RA.stdBuilder().itemInputs(new ItemStack(Blocks.wooden_slab, 1, 1))
-                .itemOutputs(
-                        new ItemStack(Items.bowl, 1),
-                        GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8).addTo(sLatheRecipes);
-
-        GT_Values.RA.stdBuilder().itemInputs(new ItemStack(Blocks.wooden_slab, 1, 2))
-                .itemOutputs(
-                        new ItemStack(Items.bowl, 1),
-                        GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8).addTo(sLatheRecipes);
-
-        GT_Values.RA.stdBuilder().itemInputs(new ItemStack(Blocks.wooden_slab, 1, 3))
-                .itemOutputs(
-                        new ItemStack(Items.bowl, 1),
-                        GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8).addTo(sLatheRecipes);
-
-        GT_Values.RA.stdBuilder().itemInputs(new ItemStack(Blocks.wooden_slab, 1, 4))
-                .itemOutputs(
-                        new ItemStack(Items.bowl, 1),
-                        GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8).addTo(sLatheRecipes);
-
-        GT_Values.RA.stdBuilder().itemInputs(new ItemStack(Blocks.wooden_slab, 1, 5))
-                .itemOutputs(
-                        new ItemStack(Items.bowl, 1),
-                        GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8).addTo(sLatheRecipes);
-
-        if (Forestry.isModLoaded()) {
-            // todo: investigate why getModItem is null here
-            for (int i = 0; i < 8; i++) {
-                GT_Values.RA.stdBuilder().itemInputs(GT_ModHandler.getModItem(Forestry.ID, "slabs1", 1, i))
-                        .itemOutputs(
-                                new ItemStack(Items.bowl, 1),
-                                GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                        .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8)
-                        .addTo(sLatheRecipes);
-
-                GT_Values.RA.stdBuilder().itemInputs(GT_ModHandler.getModItem(Forestry.ID, "slabs2", 1, i))
-                        .itemOutputs(
-                                new ItemStack(Items.bowl, 1),
-                                GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                        .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8)
-                        .addTo(sLatheRecipes);
-
-                GT_Values.RA.stdBuilder().itemInputs(GT_ModHandler.getModItem(Forestry.ID, "slabs3", 1, i))
-                        .itemOutputs(
-                                new ItemStack(Items.bowl, 1),
-                                GT_OreDictUnificator.get(OrePrefixes.dustSmall, Materials.Wood, 1))
-                        .noFluidInputs().noFluidOutputs().duration(2 * SECONDS + 10 * TICKS).eut(8)
-                        .addTo(sLatheRecipes);
-            }
-        }
     }
 }
