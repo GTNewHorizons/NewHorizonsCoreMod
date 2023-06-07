0ba908f323ccce4fd858fdaf331091ed8d76c48d
kuba6000 <kuba.123123.6000@gmail.com>
2023-06-07 21:09:49 +0200

    StackTrace is literally useless

--- src/main/java/com/dreammaster/thaumcraft/TCLoader.java
+++ src/main/java/com/dreammaster/thaumcraft/TCLoader.java
@@ -2,6 +2,9 @@ package com.dreammaster.thaumcraft;
 
 import java.util.HashSet;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
@@ -32,35 +35,43 @@ public class TCLoader {
         ResearchCategories.researchCategories.values()
                 .forEach(rc -> rc.research.forEach((k, v) -> availableResearches.add(k)));
         for (Object recipe : ThaumcraftApi.getCraftingRecipes()) {
-            String researchName = null;
-            Object output = null;
-            if (recipe instanceof InfusionRecipe) {
-                researchName = ((InfusionRecipe) recipe).getResearch();
-                output = ((InfusionRecipe) recipe).getRecipeOutput();
-            } else if (recipe instanceof IArcaneRecipe) {
-                researchName = ((IArcaneRecipe) recipe).getResearch();
-                output = ((IArcaneRecipe) recipe).getRecipeOutput();
-            } else if (recipe instanceof CrucibleRecipe) {
-                researchName = ((CrucibleRecipe) recipe).key;
-                output = ((CrucibleRecipe) recipe).getRecipeOutput();
-            } else if (recipe instanceof InfusionEnchantmentRecipe) {
-                researchName = ((InfusionEnchantmentRecipe) recipe).getResearch();
-                output = "Enchantment recipe (effectId: "
-                        + ((InfusionEnchantmentRecipe) recipe).getEnchantment().effectId
-                        + ")";
-            }
-            if (researchName == null || (!(output instanceof ItemStack) && !(output instanceof String))) continue;
-            String outputString;
-            if (output instanceof ItemStack)
-                outputString = GameRegistry.findUniqueIdentifierFor(((ItemStack) output).getItem()).toString() + ":"
-                        + ((ItemStack) output).getItemDamage();
-            else outputString = (String) output;
-            if (!availableResearches.contains(researchName)) {
-                MainRegistry.Logger.error(
-                        "WARNING!! Thaumcraft recipe for " + outputString
-                                + " has invalid research name ("
-                                + researchName
-                                + ")!");
+            try {
+                String researchName = null;
+                Object output = null;
+                if (recipe instanceof InfusionRecipe) {
+                    researchName = ((InfusionRecipe) recipe).getResearch();
+                    output = ((InfusionRecipe) recipe).getRecipeOutput();
+                } else if (recipe instanceof IArcaneRecipe) {
+                    researchName = ((IArcaneRecipe) recipe).getResearch();
+                    output = ((IArcaneRecipe) recipe).getRecipeOutput();
+                } else if (recipe instanceof CrucibleRecipe) {
+                    researchName = ((CrucibleRecipe) recipe).key;
+                    output = ((CrucibleRecipe) recipe).getRecipeOutput();
+                } else if (recipe instanceof InfusionEnchantmentRecipe) {
+                    researchName = ((InfusionEnchantmentRecipe) recipe).getResearch();
+                    output = "Enchantment recipe (effectId: "
+                            + ((InfusionEnchantmentRecipe) recipe).getEnchantment().effectId
+                            + ")";
+                }
+                if (researchName == null || (!(output instanceof ItemStack) && !(output instanceof String))) continue;
+                String outputString;
+                if (output instanceof ItemStack) {
+                    Item i = ((ItemStack) output).getItem();
+                    GameRegistry.UniqueIdentifier ui;
+                    if (i instanceof ItemBlock) ui = GameRegistry.findUniqueIdentifierFor(Block.getBlockFromItem(i));
+                    else ui = GameRegistry.findUniqueIdentifierFor(i);
+                    outputString = (ui != null ? ui.toString() : Item.getIdFromItem(i)) + ":"
+                            + ((ItemStack) output).getItemDamage();
+                } else outputString = (String) output;
+                if (!availableResearches.contains(researchName)) {
+                    MainRegistry.Logger.error(
+                            "WARNING!! Thaumcraft recipe for " + outputString
+                                    + " has invalid research name ("
+                                    + researchName
+                                    + ")!");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
