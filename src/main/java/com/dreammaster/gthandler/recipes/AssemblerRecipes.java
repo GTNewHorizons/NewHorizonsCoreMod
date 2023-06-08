84bbcb8c9698c0ba7bd094365ae0d18e669c71b1
Martin Robertz <dream-master@gmx.net>
2023-06-08 12:08:26 +0200

    add recipe for Unlighting upgrade

--- src/main/java/com/dreammaster/gthandler/recipes/AssemblerRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/AssemblerRecipes.java
@@ -5171,6 +5171,16 @@ public class AssemblerRecipes implements Runnable {
                     GT_ModHandler.getModItem(ExtraBees.ID, "alveary", 1L, 6),
                     1200,
                     120);
+
+            // Alveary Unlighting
+            GT_Values.RA.addAssemblerRecipe(
+                    new ItemStack[] { alveary, GT_ModHandler.getModItem(Forestry.ID, "thermionicTubes", 4L, 8),
+                            GT_ModHandler.getModItem(ExtraUtilities.ID, "curtains", 1L, 0),
+                            new ItemStack(Blocks.wool, 1, 15) },
+                    Materials.Honey.getFluid(7500L),
+                    GT_ModHandler.getModItem(ExtraBees.ID, "alveary", 1L, 7),
+                    1200,
+                    120);
         }
 
         if (AppliedEnergistics2.isModLoaded() && TinkerConstruct.isModLoaded()) {
