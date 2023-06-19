8683c113ea7cd4d72bc9427fdcb3050f7b8752f2
Jakub <53441451+kuba6000@users.noreply.github.com>
2023-06-19 18:50:15 +0200

    Dont put New Horizons TC Category on top (#649)
    * Dont put New Horizons TC Category on top
    
    * Make sure to load after TC

--- src/main/java/com/dreammaster/main/MainRegistry.java
+++ src/main/java/com/dreammaster/main/MainRegistry.java
@@ -86,7 +86,8 @@ import gregtech.common.items.GT_MetaGenerated_Item_01;
                 + "required-after:YAMCore@[0.5.76,);"
                 + "required-after:Baubles@[1.0.1.10,);"
                 + "after:EnderIO;"
-                + "after:HardcoreEnderExpansion;")
+                + "after:HardcoreEnderExpansion;"
+                + "after:Thaumcraft;")
 public class MainRegistry {
 
     @SidedProxy(clientSide = Refstrings.CLIENTSIDE, serverSide = Refstrings.SERVERSIDE)
@@ -348,8 +349,6 @@ public class MainRegistry {
         if (BartWorks.isModLoaded()) {
             BWGlassAdder.registerGlasses();
         }
-
-        if (Thaumcraft.isModLoaded()) TCLoader.run();
     }
 
     public static Block _mBlockBabyChest = new BlockBabyChest();
@@ -456,6 +455,8 @@ public class MainRegistry {
                     Materials.Palladium,
                     WerkstoffLoader.PDMetallicPowder.getBridgeMaterial());
         }
+
+        if (Thaumcraft.isModLoaded()) TCLoader.run();
     }
 
     @Mod.EventHandler
