dba48f819e85d9f1954d3cfef543603346206695
David Lindström <info@davidlindstrom.se>
2023-07-17 08:21:47 +0200

    Fix modpack name in the dialog that downloads mods on first startup when installed from CurseForge (#677)

--- src/main/java/com/dreammaster/coremod/DownloadProgressDialog.java
+++ src/main/java/com/dreammaster/coremod/DownloadProgressDialog.java
@@ -13,7 +13,7 @@ class DownloadProgressDialog extends JDialog {
      */
     private static final long serialVersionUID = 6041491111144915139L;
 
-    public static final String WINDOW_TITLE = "GregTech: New Horizon";
+    public static final String WINDOW_TITLE = "GT: New Horizons";
     private Thread netThread;
     private JProgressBar progressBar;
 
