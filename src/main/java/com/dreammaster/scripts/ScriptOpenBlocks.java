06c9ab5e6b5056c7bc4f9a4c338a7161ab27a2e1
chochem <40274384+chochem@users.noreply.github.com>
2023-09-04 15:47:31 +0100

    spotless

--- src/main/java/com/dreammaster/scripts/ScriptOpenBlocks.java
+++ src/main/java/com/dreammaster/scripts/ScriptOpenBlocks.java
@@ -35,9 +35,9 @@ import net.minecraftforge.fluids.FluidStack;
 import com.dreammaster.thaumcraft.TCHelper;
 
 import gregtech.api.enums.GT_Values;
-import gregtech.api.enums.TierEU;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
+import gregtech.api.enums.TierEU;
 import gregtech.api.util.GT_OreDictUnificator;
 import gregtech.api.util.GT_Utility;
 import thaumcraft.api.ThaumcraftApi;
