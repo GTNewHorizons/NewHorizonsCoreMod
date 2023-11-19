cef15642bf5260768fe551e8d43ba51caf0779d3
Raven Szewczyk <git@eigenraven.me>
2023-11-19 17:27:01 +0000

    Override mod version to 2.1.110

--- src/main/java/com/dreammaster/lib/Refstrings.java
+++ src/main/java/com/dreammaster/lib/Refstrings.java
@@ -6,7 +6,7 @@ public final class Refstrings {
 
     public static final String MODID = Mods.Names.NEW_HORIZONS_CORE_MOD;
     public static final String NAME = "GT: New Horizons";
-    public static final String VERSION = BuildVersion.VERSION;
+    public static final String VERSION = "2.1.110";
     public static final String CLIENTSIDE = "com.dreammaster.main.ClientProxy";
     public static final String SERVERSIDE = "com.dreammaster.main.CommonProxy";
     public static final String COLLECTIONID = "GTNewHorizons";
