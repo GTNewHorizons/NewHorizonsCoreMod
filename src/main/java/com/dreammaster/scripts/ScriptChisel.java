3abd8e43d8508754460dfcc8a87a5cc0850b1811
chochem <40274384+chochem@users.noreply.github.com>
2023-12-03 20:47:03 +0000

    ensure dep

--- src/main/java/com/dreammaster/scripts/ScriptChisel.java
+++ src/main/java/com/dreammaster/scripts/ScriptChisel.java
@@ -46,7 +46,8 @@ public class ScriptChisel implements IScriptLoader {
                 IronChests.ID,
                 Natura.ID,
                 Railcraft.ID,
-                TinkerConstruct.ID);
+                TinkerConstruct.ID,
+                TwilightForest.ID);
     }
 
     @Override
