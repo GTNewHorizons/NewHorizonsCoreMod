d74a5bba3a45029a05fbe6f7ebba28442277291a
Martin Robertz <dream-master@gmx.net>
2024-01-01 13:17:26 +0100

    fix

--- src/main/java/com/dreammaster/scripts/ScriptMagicBees.java
+++ src/main/java/com/dreammaster/scripts/ScriptMagicBees.java
@@ -1128,15 +1128,16 @@ public class ScriptMagicBees implements IScriptLoader {
                 "MB_ApiamancersDrainer",
                 getModItem(MagicBees.ID, "apiamancersDrainer", 1, 0, missing),
                 5,
-                new AspectList().add(Aspect.MAGIC, 100).add(Aspect.HARVEST, 75).add(Aspect.getAspect("tempus"), 50),
+                new AspectList().add(Aspect.MAGIC, 100).add(Aspect.HARVEST, 75).add(Aspect.GREED, 50)
+                        .add(Aspect.getAspect("tempus"), 25),
                 getModItem(Thaumcraft.ID, "blockEssentiaReservoir", 1, 0, missing),
                 new ItemStack[] { getModItem(Thaumcraft.ID, "blockTube", 1, 2, missing),
                         getModItem(MagicBees.ID, "pollen", 1, 0, missing),
                         getModItem(GoodGenerator.ID, "essentiaCell", 1, 1, missing),
                         getModItem(GoodGenerator.ID, "essentiaCell", 1, 1, missing),
-                        getModItem(MagicBees.ID, "pollen", 1, 1, missing),
                         getModItem(GoodGenerator.ID, "essentiaCell", 1, 1, missing),
-                        getModItem(GoodGenerator.ID, "essentiaCell", 1, 1, missing), });
+                        getModItem(GoodGenerator.ID, "essentiaCell", 1, 1, missing),
+                        getModItem(MagicBees.ID, "pollen", 1, 1, missing) });
         TCHelper.refreshResearchPages("MB_VisAuraProvider");
         TCHelper.refreshResearchPages("MB_EssenceLife");
         TCHelper.refreshResearchPages("MB_EssenceDeath");
