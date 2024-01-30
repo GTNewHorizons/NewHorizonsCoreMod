f2da673827a979691cff725b41f19334b8c1c1f3
YannickMG <yannickmg@gmail.com>
2024-01-29 19:30:50 -0500

    Reusing a safe missing item constant

--- src/main/java/com/dreammaster/recipes/ShapedIngredientsResolver.java
+++ src/main/java/com/dreammaster/recipes/ShapedIngredientsResolver.java
@@ -2,13 +2,10 @@ package com.dreammaster.recipes;
 
 import java.util.function.UnaryOperator;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-
 public class ShapedIngredientsResolver implements UnaryOperator<Object[]> {
 
     @Override
     public Object[] apply(Object[] ingredients) {
-        return new ShapedUniversalRecipe(new ItemStack(new Item()), ingredients).getInput();
+        return new ShapedUniversalRecipe(ShapedUniversalRecipe.MISSING, ingredients).getInput();
     }
 }
--- src/main/java/com/dreammaster/recipes/ShapelessIngredientsResolver.java
+++ src/main/java/com/dreammaster/recipes/ShapelessIngredientsResolver.java
@@ -1,14 +1,13 @@
 package com.dreammaster.recipes;
 
-import java.util.function.UnaryOperator;
+import static com.dreammaster.recipes.ShapedUniversalRecipe.MISSING;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
+import java.util.function.UnaryOperator;
 
 public class ShapelessIngredientsResolver implements UnaryOperator<Object[]> {
 
     @Override
     public Object[] apply(Object[] ingredients) {
-        return new ShapelessUniversalRecipe(new ItemStack(new Item()), ingredients).getInput().toArray();
+        return new ShapelessUniversalRecipe(MISSING, ingredients).getInput().toArray();
     }
 }
