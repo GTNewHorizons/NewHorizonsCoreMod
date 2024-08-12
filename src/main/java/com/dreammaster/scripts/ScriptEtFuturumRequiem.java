c41f52cc162919020c6d31cf8870c4a1f3321224
wmaxlees <max.lees@gmail.com>
2024-08-12 10:57:40 -0600

    Created a helper function for slabs and added up to Tuff

--- src/main/java/com/dreammaster/scripts/ScriptEtFuturumRequiem.java
+++ src/main/java/com/dreammaster/scripts/ScriptEtFuturumRequiem.java
@@ -66,6 +66,8 @@ public class ScriptEtFuturumRequiem implements IScriptLoader {
 
         final ItemStack RED_SANDSTONE = getModItem(EtFuturumRequiem.ID, "red_sandstone", 1, 0, missing);
         final ItemStack RED_SANDSTONE_SLAB = getModItem(EtFuturumRequiem.ID, "red_sandstone_slab", 1, 0, missing);
+        final ItemStack CUT_RED_SANDSTONE = getModItem(EtFuturumRequiem.ID, "red_sandstone", 1, 2, missing);
+        final ItemStack CUT_RED_SANDSTONE_SLAB = getModItem(EtFuturumRequiem.ID, "red_sandstone_slab", 1, 1, missing);
 
         final ItemStack PURPUR_BLOCK = getModItem(EtFuturumRequiem.ID, "purpur_block", 1, 0, missing);
         final ItemStack PURPUR_SLAB = getModItem(EtFuturumRequiem.ID, "purpur_slab", 1, 0, missing);
@@ -73,6 +75,31 @@ public class ScriptEtFuturumRequiem implements IScriptLoader {
         final ItemStack SMOOTH_STONE_SLAB = getModItem(EtFuturumRequiem.ID, "stone_slab", 1, 0, missing);
         final ItemStack MOSSY_COBBLESTONE_SLAB = getModItem(EtFuturumRequiem.ID, "stone_slab", 1, 1, missing);
         final ItemStack MOSSY_STONE_BRICK_SLAB = getModItem(EtFuturumRequiem.ID, "stone_slab", 1, 2, missing);
+
+        final ItemStack SMOOTH_QUARTZ = getModItem(EtFuturumRequiem.ID, "smooth_quartz", 1, 0, missing);
+        final ItemStack SMOOTH_QUARTZ_SLAB = getModItem(EtFuturumRequiem.ID, "smooth_quartz_slab", 1, 0, missing);
+
+        final ItemStack RED_NETHERBRICK = getModItem(EtFuturumRequiem.ID, "red_netherbrick", 1, 0, missing);
+        final ItemStack RED_NETHERBRICK_SLAB = getModItem(EtFuturumRequiem.ID, "red_netherbrick_slab", 1, 0, missing);
+
+        final ItemStack END_STONE_BRICK = getModItem(EtFuturumRequiem.ID, "end_bricks", 1, 0, missing);
+        final ItemStack END_STONE_BRICK_SLAB = getModItem(EtFuturumRequiem.ID, "end_brick_slab", 1, 0, missing);
+
+        final ItemStack COBBLED_DEEPSLATE = getModItem(EtFuturumRequiem.ID, "cobbled_deepslate", 1, 0, missing);
+        final ItemStack POLISHED_DEEPSLATE = getModItem(EtFuturumRequiem.ID, "polished_deepslate", 1, 0, missing);
+        final ItemStack DEEPSLATE_BRICKS = getModItem(EtFuturumRequiem.ID, "deepslate_bricks", 1, 0, missing);
+        final ItemStack DEEPSLATE_TILES = getModItem(EtFuturumRequiem.ID, "deepslate_bricks", 1, 2, missing);
+        final ItemStack COBBLED_DEEPSLATE_SLAB = getModItem(EtFuturumRequiem.ID, "deepslate_slab", 1, 0, missing);
+        final ItemStack POLISHED_DEEPSLATE_SLAB = getModItem(EtFuturumRequiem.ID, "deepslate_slab", 1, 1, missing);
+        final ItemStack DEEPSLATE_BRICK_SLAB = getModItem(EtFuturumRequiem.ID, "deepslate_brick_slab", 1, 0, missing);
+        final ItemStack DEEPSLATE_TILE_SLAB = getModItem(EtFuturumRequiem.ID, "deepslate_brick_slab", 1, 1, missing);
+
+        final ItemStack TUFF = getModItem(EtFuturumRequiem.ID, "tuff", 1, 0, missing);
+        final ItemStack POLISHED_TUFF = getModItem(EtFuturumRequiem.ID, "tuff", 1, 1, missing);
+        final ItemStack TUFF_BRICKS = getModItem(EtFuturumRequiem.ID, "tuff", 1, 2, missing);
+        final ItemStack TUFF_SLAB = getModItem(EtFuturumRequiem.ID, "tuff_slab", 1, 0, missing);
+        final ItemStack POLISHED_TUFF_SLAB = getModItem(EtFuturumRequiem.ID, "tuff_slab", 1, 1, missing);
+        final ItemStack TUFF_BRICK_SLAB = getModItem(EtFuturumRequiem.ID, "tuff_slab", 1, 2, missing);
 
         ItemStack crimsonWoodPlanks = CRIMSON_WOOD_PLANKS.copy();
         crimsonWoodPlanks.stackSize = 2;
@@ -94,44 +121,32 @@ public class ScriptEtFuturumRequiem implements IScriptLoader {
         bambooWoodPlanks.stackSize = 2;
         addShapelessRecipe(bambooWoodPlanks, BAMBOO_BLOCK);
 
-        ItemStack crimsonWoodSlab = CRIMSON_WOOD_SLAB.copy();
-        crimsonWoodSlab.stackSize = 2;
-        addShapelessRecipe(crimsonWoodSlab, "craftingToolSaw", null, CRIMSON_WOOD_PLANKS, null);
+        addSlabRecipe(CRIMSON_WOOD_SLAB, CRIMSON_WOOD_PLANKS);
+        addSlabRecipe(WARPED_WOOD_SLAB, WARPED_WOOD_PLANKS);
+        addSlabRecipe(MANGROVE_WOOD_SLAB, MANGROVE_WOOD_PLANKS);
+        addSlabRecipe(CHERRY_WOOD_SLAB, CHERRY_WOOD_PLANKS);
+        addSlabRecipe(BAMBOO_WOOD_SLAB, BAMBOO_WOOD_PLANKS);
+        addSlabRecipe(RED_SANDSTONE_SLAB, RED_SANDSTONE);
+        addSlabRecipe(CUT_RED_SANDSTONE_SLAB, CUT_RED_SANDSTONE);
+        addSlabRecipe(PURPUR_SLAB, PURPUR_BLOCK);
+        addSlabRecipe(SMOOTH_STONE_SLAB, "stone");
+        addSlabRecipe(MOSSY_COBBLESTONE_SLAB, getModItem(Minecraft.ID, "mossy_cobblestone", 1, 0, missing));
+        addSlabRecipe(MOSSY_STONE_BRICK_SLAB, getModItem(Minecraft.ID, "stonebrick", 1, 1, missing));
+        addSlabRecipe(SMOOTH_QUARTZ_SLAB, SMOOTH_QUARTZ);
+        addSlabRecipe(RED_NETHERBRICK_SLAB, RED_NETHERBRICK);
+        addSlabRecipe(END_STONE_BRICK_SLAB, END_STONE_BRICK);
+        addSlabRecipe(COBBLED_DEEPSLATE_SLAB, COBBLED_DEEPSLATE);
+        addSlabRecipe(POLISHED_DEEPSLATE_SLAB, POLISHED_DEEPSLATE);
+        addSlabRecipe(DEEPSLATE_BRICK_SLAB, DEEPSLATE_BRICKS);
+        addSlabRecipe(DEEPSLATE_TILE_SLAB, DEEPSLATE_TILES);
+        addSlabRecipe(TUFF_SLAB, TUFF);
+        addSlabRecipe(POLISHED_TUFF_SLAB, POLISHED_TUFF);
+        addSlabRecipe(TUFF_BRICK_SLAB, TUFF_BRICKS);
+    }
 
-        ItemStack warpedWoodSlab = WARPED_WOOD_SLAB.copy();
-        warpedWoodSlab.stackSize = 2;
-        addShapelessRecipe(warpedWoodSlab, "craftingToolSaw", null, WARPED_WOOD_PLANKS, null);
-
-        ItemStack mangroveWoodSlab = MANGROVE_WOOD_SLAB.copy();
-        mangroveWoodSlab.stackSize = 2;
-        addShapelessRecipe(mangroveWoodSlab, "craftingToolSaw", null, MANGROVE_WOOD_PLANKS, null);
-
-        ItemStack cherryWoodSlab = CHERRY_WOOD_SLAB.copy();
-        cherryWoodSlab.stackSize = 2;
-        addShapelessRecipe(cherryWoodSlab, "craftingToolSaw", null, CHERRY_WOOD_PLANKS, null);
-
-        ItemStack bambooWoodSlab = BAMBOO_WOOD_SLAB.copy();
-        bambooWoodSlab.stackSize = 2;
-        addShapelessRecipe(bambooWoodSlab, "craftingToolSaw", null, BAMBOO_WOOD_PLANKS, null);
-
-        ItemStack redSandstoneSlab = RED_SANDSTONE_SLAB.copy();
-        redSandstoneSlab.stackSize = 2;
-        addShapelessRecipe(redSandstoneSlab, "craftingToolSaw", null, RED_SANDSTONE, null);
-
-        ItemStack purpurSlab = PURPUR_SLAB.copy();
-        purpurSlab.stackSize = 2;
-        addShapelessRecipe(purpurSlab, "craftingToolSaw", null, PURPUR_BLOCK, null);
-
-        ItemStack smoothStoneSlab = SMOOTH_STONE_SLAB.copy();
-        smoothStoneSlab.stackSize = 2;
-        addShapelessRecipe(smoothStoneSlab, "craftingToolSaw", null, "stone", null);
-
-        ItemStack mossyCobblestoneSlab = MOSSY_COBBLESTONE_SLAB.copy();
-        mossyCobblestoneSlab.stackSize = 2;
-        addShapelessRecipe(mossyCobblestoneSlab, "craftingToolSaw", null, getModItem(Minecraft.ID, "mossy_cobblestone", 1, 0, missing), null);
-        
-        ItemStack mossyStoneBrickSlab = MOSSY_STONE_BRICK_SLAB.copy();
-        mossyStoneBrickSlab.stackSize = 2;
-        addShapelessRecipe(mossyStoneBrickSlab, "craftingToolSaw", null, getModItem(Minecraft.ID, "stonebrick", 1, 1, missing), null);
+    protected void addSlabRecipe(ItemStack result, Object ingredient) {
+        ItemStack output = result.copy();
+        output.stackSize = 2;
+        addShapelessRecipe(output, "craftingToolSaw", null, ingredient, null);
     }
 };
