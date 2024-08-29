14d469c96e0a823bbe2f41fe5cd1b04a61656aa2
Mary Hopson <doyoumined@gmail.com>
2024-08-29 17:16:02 -0400

    Compressor recipes

--- src/main/java/com/dreammaster/gthandler/recipes/AssemblerRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/AssemblerRecipes.java
@@ -37,6 +37,7 @@ import com.github.bartimaeusnek.bartworks.common.loaders.ItemRegistry;
 import com.github.bartimaeusnek.bartworks.system.material.WerkstoffLoader;
 import common.TileEntities;
 
+import goodgenerator.items.MyMaterial;
 import gregtech.api.enums.GT_Values;
 import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
@@ -2262,13 +2263,30 @@ public class AssemblerRecipes implements Runnable {
                 .itemInputs(
                         GT_OreDictUnificator.get(OrePrefixes.plate, Materials.TungstenSteel, 6L),
                         GT_OreDictUnificator.get(OrePrefixes.frameGt, Materials.Titanium, 1L))
-                .itemOutputs(getModItem(GregTech.ID, "gt.blockcasings10", 1, 0)).duration(2 * SECONDS + 10 * TICKS)
+                .itemOutputs(ItemList.Casing_Electromagnetic_Separator.get(1)).duration(2 * SECONDS + 10 * TICKS)
                 .eut(16).addTo(assemblerRecipes);
 
         // Laser Containment Casing
         GT_Values.RA.stdBuilder().itemInputs(STELLITE.getPlate(6), NITINOL_60.getFrameBox(1))
-                .itemOutputs(getModItem(GregTech.ID, "gt.blockcasings10", 1, 1)).duration(2 * SECONDS + 10 * TICKS)
-                .eut(16).addTo(assemblerRecipes);
+                .itemOutputs(ItemList.Casing_Laser.get(1)).duration(2 * SECONDS + 10 * TICKS).eut(16)
+                .addTo(assemblerRecipes);
+
+        // Electric Compressor Casing
+        GT_Values.RA.stdBuilder()
+                .itemInputs(
+                        MyMaterial.incoloy903.get(OrePrefixes.plate, 6),
+                        MyMaterial.incoloy903.get(OrePrefixes.block, 1))
+                .itemOutputs(ItemList.Compressor_Casing.get(1)).duration(2 * SECONDS + 10 * TICKS).eut(16)
+                .addTo(assemblerRecipes);
+
+        // Compression Pipe Casing
+        GT_Values.RA.stdBuilder()
+                .itemInputs(
+                        MyMaterial.incoloy903.get(OrePrefixes.plate, 4),
+                        MyMaterial.incoloy903.get(OrePrefixes.gearGt, 1),
+                        MyMaterial.incoloy903.get(OrePrefixes.pipeMedium, 4))
+                .itemOutputs(ItemList.Compressor_Pipe_Casing.get(1)).duration(2 * SECONDS + 10 * TICKS).eut(16)
+                .addTo(assemblerRecipes);
 
         // Iron Electromagnet
         GT_Values.RA.stdBuilder()
