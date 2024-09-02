5d3745eb88ca22b193f695a127b12cd7a366f5d0
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-09-03 01:46:27 +0200

    star imports

--- src/main/java/com/dreammaster/coremod/transformers/recipenukers/AdvancedSolarPanelTransformer.java
+++ src/main/java/com/dreammaster/coremod/transformers/recipenukers/AdvancedSolarPanelTransformer.java
@@ -3,7 +3,12 @@ package com.dreammaster.coremod.transformers.recipenukers;
 import java.util.ListIterator;
 
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.*;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import com.dreammaster.coremod.DreamCoreMod;
 import com.dreammaster.coremod.IDreamTransformer;
