8e2615b9a3de5f4b630711405b44f6642ff2fe49
StaffiX <staffixhun@gmail.com>
2024-09-04 17:15:17 +0200

    Indium comb recipes for existing indium sources

--- src/main/java/com/dreammaster/gthandler/GT_MachineRecipeLoader.java
+++ src/main/java/com/dreammaster/gthandler/GT_MachineRecipeLoader.java
@@ -1,43 +1,6 @@
 package com.dreammaster.gthandler;
 
-import com.dreammaster.gthandler.recipes.AlloySmelterRecipes;
-import com.dreammaster.gthandler.recipes.ArcFurnaceRecipes;
-import com.dreammaster.gthandler.recipes.AssemblerRecipes;
-import com.dreammaster.gthandler.recipes.AssemblingLineRecipes;
-import com.dreammaster.gthandler.recipes.AutoclaveRecipes;
-import com.dreammaster.gthandler.recipes.BendingMachineRecipes;
-import com.dreammaster.gthandler.recipes.BlastFurnaceRecipes;
-import com.dreammaster.gthandler.recipes.BrewingMachineRecipes;
-import com.dreammaster.gthandler.recipes.CannerRecipes;
-import com.dreammaster.gthandler.recipes.CentrifugeRecipes;
-import com.dreammaster.gthandler.recipes.ChemicalBathRecipes;
-import com.dreammaster.gthandler.recipes.ChemicalReactorRecipes;
-import com.dreammaster.gthandler.recipes.CircuitAssemblerRecipes;
-import com.dreammaster.gthandler.recipes.CompressorRecipes;
-import com.dreammaster.gthandler.recipes.CrackingRecipes;
-import com.dreammaster.gthandler.recipes.CuttingMachineRecipes;
-import com.dreammaster.gthandler.recipes.DistillationRecipes;
-import com.dreammaster.gthandler.recipes.ElectrolyzerRecipes;
-import com.dreammaster.gthandler.recipes.ExtractorRecipes;
-import com.dreammaster.gthandler.recipes.ExtruderRecipes;
-import com.dreammaster.gthandler.recipes.FermenterRecipes;
-import com.dreammaster.gthandler.recipes.FluidCannerRecipes;
-import com.dreammaster.gthandler.recipes.FluidExtractorRecipes;
-import com.dreammaster.gthandler.recipes.FluidHeaterRecipes;
-import com.dreammaster.gthandler.recipes.FluidSolidifierRecipes;
-import com.dreammaster.gthandler.recipes.ForgeHammerRecipes;
-import com.dreammaster.gthandler.recipes.FormingPressRecipes;
-import com.dreammaster.gthandler.recipes.ImplosionCompressorRecipes;
-import com.dreammaster.gthandler.recipes.LaserEngraverRecipes;
-import com.dreammaster.gthandler.recipes.LatheRecipes;
-import com.dreammaster.gthandler.recipes.MaceratorRecipes;
-import com.dreammaster.gthandler.recipes.MixerRecipes;
-import com.dreammaster.gthandler.recipes.NeutroniumCompressorRecipes;
-import com.dreammaster.gthandler.recipes.RecipeRemoval;
-import com.dreammaster.gthandler.recipes.SifterRecipes;
-import com.dreammaster.gthandler.recipes.SpaceAssemblerRecipes;
-import com.dreammaster.gthandler.recipes.VacuumFreezerRecipes;
-import com.dreammaster.gthandler.recipes.WiremillRecipes;
+import com.dreammaster.gthandler.recipes.*;
 
 public class GT_MachineRecipeLoader implements Runnable {
 
@@ -82,6 +45,7 @@ public class GT_MachineRecipeLoader implements Runnable {
         new WiremillRecipes().run();
         new CrackingRecipes().run();
         new SpaceAssemblerRecipes().run();
+        new VacuumFurnaceRecipes().run();
     }
 
 }
--- src/main/java/com/dreammaster/gthandler/recipes/CentrifugeRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/CentrifugeRecipes.java
@@ -27,6 +27,10 @@ import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
 import gregtech.api.enums.TierEU;
+import bartworks.system.material.WerkstoffLoader;
+import goodgenerator.items.GGMaterial;
+import gregtech.common.items.CombType;
+import gregtech.loaders.misc.GTBees;
 import gregtech.api.util.GTModHandler;
 import gregtech.api.util.GTOreDictUnificator;
 import gregtech.api.util.GTUtility;
@@ -590,24 +594,22 @@ public class CentrifugeRecipes implements Runnable {
                         GTOreDictUnificator.get(OrePrefixes.dust, Materials.Electrum, 1L))
                 .duration(40 * SECONDS).eut(TierEU.RECIPE_LV).addTo(centrifugeRecipes);
 
-        
         GTValues.RA.stdBuilder().
-            itemInputs(GTUtility.getIntegratedCircuit(2),
-                       GTBees.combs.getStackForType(CombType.INDIUM, 8))
-            .fluidInputs(GGMaterial.thoriumBasedLiquidFuelDepleted.getFluidOrGas(1000))
-            .itemOutputs(
-                WerkstoffLoader.Thorium232.get(OrePrefixes.dust, 64),
-                WerkstoffLoader.Thorium232.get(OrePrefixes.dust, 16),
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Praseodymium, 64),
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Praseodymium, 32),
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Boron, 2),
-                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 12))
-            .outputChances(10000, 8000, 10000, 8000, 3000, 5000)
-            .duration(1 * MINUTES + 15 * SECONDS)
-            .eut(TierEU.RECIPE_EV / 2)
-            .addTo(centrifugeRecipes);
+                itemInputs(GTUtility.getIntegratedCircuit(2),
+                        GTBees.combs.getStackForType(CombType.INDIUM, 8))
+                .fluidInputs(GGMaterial.thoriumBasedLiquidFuelDepleted.getFluidOrGas(1000))
+                .itemOutputs(
+                        WerkstoffLoader.Thorium232.get(OrePrefixes.dust, 64),
+                        WerkstoffLoader.Thorium232.get(OrePrefixes.dust, 16),
+                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Praseodymium, 64),
+                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Praseodymium, 32),
+                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Boron, 2),
+                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 12))
+                .outputChances(10000, 8000, 10000, 8000, 3000, 5000)
+                .duration(1 * MINUTES + 15 * SECONDS)
+                .eut(TierEU.RECIPE_EV / 2)
+                .addTo(centrifugeRecipes);
 
-        
         GTValues.RA.stdBuilder().itemInputs(ItemList.Cell_Air.get(5L))
                 .itemOutputs(Materials.Oxygen.getCells(1), ItemList.Cell_Empty.get(4L))
                 .fluidOutputs(Materials.Nitrogen.getGas(3900L)).duration(1 * MINUTES + 20 * SECONDS).eut(8)
