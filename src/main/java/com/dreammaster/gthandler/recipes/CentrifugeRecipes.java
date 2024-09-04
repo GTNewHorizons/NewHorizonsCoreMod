7312983c265df328f80cba22adc79f073f537f43
StaffiX <32968022+StaffiX@users.noreply.github.com>
2024-09-04 14:20:39 +0200

    thorium fuel indium comb

--- src/main/java/com/dreammaster/gthandler/recipes/CentrifugeRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/CentrifugeRecipes.java
@@ -590,6 +590,24 @@ public class CentrifugeRecipes implements Runnable {
                         GTOreDictUnificator.get(OrePrefixes.dust, Materials.Electrum, 1L))
                 .duration(40 * SECONDS).eut(TierEU.RECIPE_LV).addTo(centrifugeRecipes);
 
+        
+        GTValues.RA.stdBuilder().
+            itemInputs(GTUtility.getIntegratedCircuit(2),
+                       GTBees.combs.getStackForType(CombType.INDIUM, 8))
+            .fluidInputs(GGMaterial.thoriumBasedLiquidFuelDepleted.getFluidOrGas(1000))
+            .itemOutputs(
+                WerkstoffLoader.Thorium232.get(OrePrefixes.dust, 64),
+                WerkstoffLoader.Thorium232.get(OrePrefixes.dust, 16),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Praseodymium, 64),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Praseodymium, 32),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Boron, 2),
+                GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 12))
+            .outputChances(10000, 8000, 10000, 8000, 3000, 5000)
+            .duration(1 * MINUTES + 15 * SECONDS)
+            .eut(TierEU.RECIPE_EV / 2)
+            .addTo(centrifugeRecipes);
+
+        
         GTValues.RA.stdBuilder().itemInputs(ItemList.Cell_Air.get(5L))
                 .itemOutputs(Materials.Oxygen.getCells(1), ItemList.Cell_Empty.get(4L))
                 .fluidOutputs(Materials.Nitrogen.getGas(3900L)).duration(1 * MINUTES + 20 * SECONDS).eut(8)
