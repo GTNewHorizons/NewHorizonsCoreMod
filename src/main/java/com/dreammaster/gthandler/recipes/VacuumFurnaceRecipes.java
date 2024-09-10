baff68a69f070ea5e29750b647e25c419ee73278
chochem <40274384+chochem@users.noreply.github.com>
2024-09-10 19:40:30 +0100

    add missing ismodloaded checks for forestry (#992)

--- src/main/java/com/dreammaster/gthandler/recipes/VacuumFurnaceRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/VacuumFurnaceRecipes.java
@@ -1,5 +1,6 @@
 package com.dreammaster.gthandler.recipes;
 
+import static gregtech.api.enums.Mods.Forestry;
 import static gregtech.api.util.GTRecipeBuilder.MINUTES;
 import static gregtech.api.util.GTRecipeConstants.COIL_HEAT;
 import static gtPlusPlus.api.recipe.GTPPRecipeMaps.vacuumFurnaceRecipes;
@@ -23,41 +24,43 @@ public class VacuumFurnaceRecipes implements Runnable {
     @Override
     public void run() {
 
-        GTValues.RA.stdBuilder()
-                .itemInputs(
-                        GTUtility.getIntegratedCircuit(12),
-                        GTBees.combs.getStackForType(CombType.INDIUM, 64),
-                        GTBees.combs.getStackForType(CombType.INDIUM, 64),
-                        GTBees.combs.getStackForType(CombType.INDIUM, 64),
-                        GTBees.combs.getStackForType(CombType.INDIUM, 64))
-                .itemOutputs(
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 64L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 64L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 52L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 64L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 56L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 64L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 64L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 64L),
-                        MaterialsElements.getInstance().GERMANIUM.getDust(15))
-                .fluidInputs(FluidUtils.getFluidStack(SphaleriteFlotationFroth, 4000))
-                .fluidOutputs(FluidUtils.getFluidStack(AgriculturalChem.RedMud, 2000), FluidUtils.getWater(2000))
-                .eut((int) TierEU.RECIPE_LuV).metadata(COIL_HEAT, 5500).duration(2 * MINUTES)
-                .addTo(vacuumFurnaceRecipes);
+        if (Forestry.isModLoaded()) {
+            GTValues.RA.stdBuilder()
+                    .itemInputs(
+                            GTUtility.getIntegratedCircuit(12),
+                            GTBees.combs.getStackForType(CombType.INDIUM, 64),
+                            GTBees.combs.getStackForType(CombType.INDIUM, 64),
+                            GTBees.combs.getStackForType(CombType.INDIUM, 64),
+                            GTBees.combs.getStackForType(CombType.INDIUM, 64))
+                    .itemOutputs(
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 64L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 64L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Zinc, 52L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 64L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 56L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 64L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 64L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 64L),
+                            MaterialsElements.getInstance().GERMANIUM.getDust(15))
+                    .fluidInputs(FluidUtils.getFluidStack(SphaleriteFlotationFroth, 4000))
+                    .fluidOutputs(FluidUtils.getFluidStack(AgriculturalChem.RedMud, 2000), FluidUtils.getWater(2000))
+                    .eut((int) TierEU.RECIPE_LuV).metadata(COIL_HEAT, 5500).duration(2 * MINUTES)
+                    .addTo(vacuumFurnaceRecipes);
 
-        GTValues.RA.stdBuilder()
-                .itemInputs(GTUtility.getIntegratedCircuit(13), GTBees.combs.getStackForType(CombType.INDIUM, 40))
-                .itemOutputs(
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 64L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 64L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 52L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 64L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 56L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Cadmium, 50L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 30L))
-                .fluidInputs(FluidUtils.getFluidStack(ChalcopyriteFlotationFroth, 4000))
-                .fluidOutputs(FluidUtils.getFluidStack(AgriculturalChem.RedMud, 2000), FluidUtils.getWater(2000))
-                .eut((int) TierEU.RECIPE_IV).metadata(COIL_HEAT, 4500).duration(2 * MINUTES)
-                .addTo(vacuumFurnaceRecipes);
+            GTValues.RA.stdBuilder()
+                    .itemInputs(GTUtility.getIntegratedCircuit(13), GTBees.combs.getStackForType(CombType.INDIUM, 40))
+                    .itemOutputs(
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 64L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 64L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Copper, 52L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 64L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Iron, 56L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Cadmium, 50L),
+                            GTOreDictUnificator.get(OrePrefixes.dust, Materials.Indium, 30L))
+                    .fluidInputs(FluidUtils.getFluidStack(ChalcopyriteFlotationFroth, 4000))
+                    .fluidOutputs(FluidUtils.getFluidStack(AgriculturalChem.RedMud, 2000), FluidUtils.getWater(2000))
+                    .eut((int) TierEU.RECIPE_IV).metadata(COIL_HEAT, 4500).duration(2 * MINUTES)
+                    .addTo(vacuumFurnaceRecipes);
+        }
     }
 }
