f128d17404d8991b8f254a4f937ebe4e2d000b29
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-10-11 13:48:09 +0200

    assign locals for the links

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -17,24 +17,27 @@ public class LoginHandler {
     @SuppressWarnings("unused")
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
+        final String WIKI_LINK = "https://gtnh.miraheze.org/wiki/";
+        final String ISSUE_TRACKER_LINK = "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues";
+        final String DISCORD_LINK = "https://discord.gg/gtnh";
         event.player.addChatMessage(new ChatComponentText(GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------"));
         event.player.addChatMessage(new ChatComponentText(BOLD + "Welcome to Gregtech: New Horizons " + GREEN + ModPackVersion));
         event.player.addChatMessage(new ChatComponentText(BLUE + "The Quest Book has a shortcut key, check your keybindings."));
         event.player.addChatMessage(new ChatComponentText(DARK_GREEN + "GTNH WIKI link : ")
-                .appendSibling(new ChatComponentText(DARK_GREEN + "https://gtnh.miraheze.org/wiki/")
+                .appendSibling(new ChatComponentText(DARK_GREEN + WIKI_LINK)
                         .setChatStyle(new ChatStyle()
                                 .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(YELLOW + "Click to open the wiki!")))
-                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://gtnh.miraheze.org/wiki/")))));
+                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, WIKI_LINK)))));
         event.player.addChatMessage(new ChatComponentText(GREEN + "Please report bugs on Github :"));
-        event.player.addChatMessage(new ChatComponentText(GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues")
+        event.player.addChatMessage(new ChatComponentText(GOLD + ISSUE_TRACKER_LINK)
                 .setChatStyle(new ChatStyle()
                         .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(YELLOW + "Click to open the Github!")))
-                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues"))));
+                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, ISSUE_TRACKER_LINK))));
         event.player.addChatMessage(new ChatComponentText(BLUE + "Visit our discord at ")
-                .appendSibling(new ChatComponentText(BLUE + "https://discord.gg/gtnh")
+                .appendSibling(new ChatComponentText(BLUE + DISCORD_LINK)
                         .setChatStyle(new ChatStyle()
                                 .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(YELLOW + "Click to open discord!")))
-                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/gtnh")))));
+                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, DISCORD_LINK)))));
         event.player.addChatMessage(new ChatComponentText(GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------"));
     }
 }
