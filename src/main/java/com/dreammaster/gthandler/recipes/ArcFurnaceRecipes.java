86eeefd744651621bacb25fdfe04f61f479fdcc6
StaffiX <staffixhun@gmail.com>
2024-10-11 21:12:22 +0200

    add glass to plasma arc furnace

--- src/main/java/com/dreammaster/gthandler/recipes/ArcFurnaceRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/ArcFurnaceRecipes.java
@@ -1,9 +1,15 @@
 package com.dreammaster.gthandler.recipes;
 
 import static gregtech.api.enums.Mods.IndustrialCraft2;
+import static gregtech.api.enums.Mods.Railcraft;
+import static gregtech.api.util.GTModHandler.getModItem;
 import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 import static gregtech.api.util.GTRecipeBuilder.TICKS;
+import static gregtech.api.util.GTRecipeConstants.RECYCLE;
 import static gregtech.api.util.GTRecipeConstants.UniversalArcFurnace;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 
 import gregtech.api.enums.GTValues;
 import gregtech.api.enums.Materials;
@@ -18,5 +24,21 @@ public class ArcFurnaceRecipes implements Runnable {
         GTValues.RA.stdBuilder().itemInputs(GTModHandler.getModItem(IndustrialCraft2.ID, "blockMiningPipe", 1L))
                 .itemOutputs(GTOreDictUnificator.get(OrePrefixes.nugget, Materials.Steel, 2L))
                 .duration(2 * SECONDS + 10 * TICKS).eut(90).addTo(UniversalArcFurnace);
+
+        GTValues.RA.stdBuilder().itemInputs(getModItem(Railcraft.ID, "machine.zeta", 1L, 7))
+                .itemOutputs(
+                        GTOreDictUnificator.get(OrePrefixes.ingot, Materials.Titanium, 1L),
+                        GTOreDictUnificator.get(OrePrefixes.dustTiny, Materials.Glass, 3))
+                .duration(13 * SECONDS + 10 * TICKS).eut(210).metadata(RECYCLE, true).addTo(UniversalArcFurnace);
+
+        GTValues.RA.stdBuilder().itemInputs(new ItemStack(Blocks.sand, 1, 0))
+                .itemOutputs(new ItemStack(Blocks.glass, 2)).duration(1 * SECONDS).eut(256).addTo(UniversalArcFurnace);
+
+        // red sand
+        GTValues.RA.stdBuilder().itemInputs(new ItemStack(Blocks.sand, 1, 1))
+                .itemOutputs(
+                        new ItemStack(Blocks.glass, 2),
+                        GTOreDictUnificator.get(OrePrefixes.dustTiny, Materials.Ash, 1))
+                .duration(1 * SECONDS).eut(256).addTo(UniversalArcFurnace);
     }
 }
