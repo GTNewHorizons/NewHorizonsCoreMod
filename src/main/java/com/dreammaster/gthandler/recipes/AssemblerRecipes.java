31cda019742646384644121aa477c68b4056c590
Dioxop <88603010+Dioxop@users.noreply.github.com>
2024-10-21 21:25:45 +0200

    Added Multiple Steam Machine Hulls
    - Added Bronze Hull Assembler Recipe
    - Added Bricked Bronze Hull Assembler Recipe
    - Added Steel Hull Assembler Recipe
    - Added Bricked Wrought Iron Hull Assembler Recipe

--- src/main/java/com/dreammaster/gthandler/recipes/AssemblerRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/AssemblerRecipes.java
@@ -31,6 +31,7 @@ import java.util.Map;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
@@ -97,6 +98,42 @@ public class AssemblerRecipes implements Runnable {
         makeElectricMachinePartRecipes();
         makeCircuitPartRecipes();
 
+        // Bronze Hull
+        GTValues.RA.stdBuilder()
+                .itemInputs(GTOreDictUnificator.get(OrePrefixes.plate, Materials.Bronze, 8))
+                .itemOutputs(GTModHandler.getModItem("gregtech", "gt.blockmachines", 1, 1))
+                .fluidInputs(FluidRegistry.getFluidStack("refinedglue", 288))
+                .duration(2 * SECONDS + 10).eut(TierEU.RECIPE_LV)
+                .addTo(assemblerRecipes);
+
+        // Bricked Bronze Hull
+        GTValues.RA.stdBuilder()
+                .itemInputs(
+                        GTOreDictUnificator.get(OrePrefixes.plate, Materials.Bronze, 5),
+                        new ItemStack(Blocks.brick_block, 3))
+                .itemOutputs(GTModHandler.getModItem("gregtech", "gt.blockmachines", 1, 2))
+                .fluidInputs(FluidRegistry.getFluidStack("refinedglue", 288))
+                .duration(2 * SECONDS + 10).eut(TierEU.RECIPE_LV)
+                .addTo(assemblerRecipes);
+
+        // Steel Hull
+        GTValues.RA.stdBuilder()
+                .itemInputs(GTOreDictUnificator.get(OrePrefixes.plate, Materials.Steel, 8))
+                .itemOutputs(GTModHandler.getModItem("gregtech", "gt.blockmachines", 1, 3))
+                .fluidInputs(FluidRegistry.getFluidStack("refinedglue", 288))
+                .duration(2 * SECONDS + 10).eut(TierEU.RECIPE_LV)
+                .addTo(assemblerRecipes);
+
+        // Bricked Wrought Iron Hull
+        GTValues.RA.stdBuilder()
+                .itemInputs(
+                        GTOreDictUnificator.get(OrePrefixes.plate, Materials.WroughtIron, 5),
+                        new ItemStack(Blocks.brick_block, 3))
+                .itemOutputs(GTModHandler.getModItem("gregtech", "gt.blockmachines", 1, 4))
+                .fluidInputs(FluidRegistry.getFluidStack("refinedglue", 288))
+                .duration(2 * SECONDS + 10).eut(TierEU.RECIPE_LV)
+                .addTo(assemblerRecipes);
+
         GTValues.RA.stdBuilder()
                 .itemInputs(
                         GTModHandler.getModItem(IndustrialCraft2.ID, "blockAlloyGlass", 1L, 0),
@@ -104,6 +141,7 @@ public class AssemblerRecipes implements Runnable {
                         GTUtility.getIntegratedCircuit(1))
                 .itemOutputs(CustomItemList.Empty180SpCell.get(1L)).duration(5 * SECONDS).eut(TierEU.RECIPE_MV)
                 .addTo(assemblerRecipes);
+
 
         GTValues.RA.stdBuilder()
                 .itemInputs(
@@ -10278,6 +10316,7 @@ public class AssemblerRecipes implements Runnable {
                         .itemOutputs(GTModHandler.getModItem(Computronics.ID, "computronics.dockingUpgrade", 1L, 0))
                         .fluidInputs(tMat.getMolten(144L * tMultiplier / 2L)).duration(12 * SECONDS + 10 * TICKS)
                         .eut(TierEU.RECIPE_HV).addTo(assemblerRecipes);
+
             }
         }
     }
