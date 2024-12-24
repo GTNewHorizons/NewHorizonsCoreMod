c63ed801fc35e6a293be2b095852978fd7d83327
chochem <40274384+chochem@users.noreply.github.com>
2024-12-24 12:51:02 +0000

    add some recipes

---
+++ src/main/java/com/dreammaster/scripts/ScriptEFR.java
@@ -0,0 +1,57 @@
+package com.dreammaster.scripts;
+
+import static gregtech.api.enums.Mods.EtFuturumRequiem;
+import static gregtech.api.enums.Mods.IndustrialCraft2;
+import static gregtech.api.enums.Mods.Minecraft;
+
+import java.util.Arrays;
+import java.util.List;
+
+import gregtech.api.enums.ItemList;
+import gregtech.api.util.GTModHandler;
+
+public class ScriptEFR implements IScriptLoader {
+
+    @Override
+    public String getScriptName() {
+        return "Et Futurum Requiem";
+    }
+
+    @Override
+    public List<String> getDependencies() {
+        return Arrays.asList(EtFuturumRequiem.ID, IndustrialCraft2.ID);
+    }
+
+    @Override
+    public void loadRecipes() {
+        long bits = GTModHandler.RecipeBits.NOT_REMOVABLE | GTModHandler.RecipeBits.REVERSIBLE
+                | GTModHandler.RecipeBits.BUFFERED;
+        GTModHandler.addCraftingRecipe(
+                GTModHandler.getModItem(EtFuturumRequiem.ID, "observer", 1L),
+                bits,
+                new Object[] { "AEA", "BCD", "AAA", 'A', "cobblestone", 'B',
+                        GTModHandler.getModItem(Minecraft.ID, "comparator", 1, 0), 'C', ItemList.Sensor_LV, 'D',
+                        ItemList.Emitter_LV, 'E', "gearGtSmallAnyIron" });
+
+        GTModHandler.addCraftingRecipe(
+                GTModHandler.getModItem(EtFuturumRequiem.ID, "cherry_trapdoor", 1L),
+                bits,
+                new Object[] { "ABA", "BCB", "ABA", 'A',
+                        GTModHandler.getModItem(EtFuturumRequiem.ID, "wood_slab", 1L, 3), 'B', "stickWood", 'C',
+                        "itemFlint" });
+
+        GTModHandler.addCraftingRecipe(
+                GTModHandler.getModItem(EtFuturumRequiem.ID, "cherry_trapdoor", 2L),
+                bits,
+                new Object[] { "ABA", "BCB", "ABA", 'A',
+                        GTModHandler.getModItem(EtFuturumRequiem.ID, "wood_slab", 1L, 3), 'B', "stickWood", 'C',
+                        "screwIron" });
+
+        GTModHandler.addCraftingRecipe(
+                GTModHandler.getModItem(EtFuturumRequiem.ID, "cherry_trapdoor", 3L),
+                bits,
+                new Object[] { "ABA", "BCB", "ABA", 'A',
+                        GTModHandler.getModItem(EtFuturumRequiem.ID, "wood_slab", 1L, 3), 'B', "stickWood", 'C',
+                        "screwSteel" });
+    }
+}
