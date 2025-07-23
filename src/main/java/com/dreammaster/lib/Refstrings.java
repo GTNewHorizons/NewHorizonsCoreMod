cd7811e031403cdc1c0ef2529af9a6529febad79
DylanTaylor1 <89085119+DylanTaylor1@users.noreply.github.com>
2025-07-23 12:25:52 +0200

    Update GTNH game join message (#1308)
    Co-authored-by: Alexdoru <57050655+Alexdoru@users.noreply.github.com>

--- src/main/java/com/dreammaster/lib/Refstrings.java
+++ src/main/java/com/dreammaster/lib/Refstrings.java
@@ -10,5 +10,9 @@ public final class Refstrings {
     public static final String SERVERSIDE = "com.dreammaster.main.CommonProxy";
     public static final String COLLECTIONID = "GTNewHorizons";
 
+    public static final String WIKI_LINK = "https://wiki.gtnewhorizons.com/wiki/";
+    public static final String ISSUE_TRACKER_LINK = "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues";
+    public static final String DISCORD_LINK = "https://discord.gg/gtnh";
+
     private Refstrings() {}
 }
