f2b5f22e58319636d679ff19538fb2d2f8513564
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-23 09:37:14 +0200

    extract common links strings to the same class

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -9,6 +9,8 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.StatCollector;
 
+import com.dreammaster.lib.Refstrings;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
@@ -17,9 +19,6 @@ public class LoginHandler {
     // spotless:off
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        final String WIKI_LINK = "https://wiki.gtnewhorizons.com/wiki/";
-        final String ISSUE_TRACKER_LINK = "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/issues";
-        final String DISCORD_LINK = "https://discord.gg/gtnh";
         final String WELCOME       = StatCollector.translateToLocal("dreamcraft.welcome.welcome");
         final String QUESTBOOK     = StatCollector.translateToLocal("dreamcraft.welcome.questbook");
         final String GTNH_WIKI     = StatCollector.translateToLocal("dreamcraft.welcome.gtnh_wiki");
@@ -32,20 +31,20 @@ public class LoginHandler {
         event.player.addChatMessage(new ChatComponentText(BOLD + WELCOME + " " + GREEN + ModPackVersion));
         event.player.addChatMessage(new ChatComponentText(BLUE + QUESTBOOK));
         event.player.addChatMessage(new ChatComponentText(DARK_GREEN + GTNH_WIKI + " ")
-                .appendSibling(new ChatComponentText(DARK_GREEN + WIKI_LINK)
+                .appendSibling(new ChatComponentText(DARK_GREEN + Refstrings.WIKI_LINK)
                         .setChatStyle(new ChatStyle()
                                 .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(YELLOW + CLICK_WIKI)))
-                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, WIKI_LINK)))));
+                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, Refstrings.WIKI_LINK)))));
         event.player.addChatMessage(new ChatComponentText(GREEN + REPORT_BUG));
-        event.player.addChatMessage(new ChatComponentText(GOLD + ISSUE_TRACKER_LINK)
+        event.player.addChatMessage(new ChatComponentText(GOLD + Refstrings.ISSUE_TRACKER_LINK)
                 .setChatStyle(new ChatStyle()
                         .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(YELLOW + CLICK_GITHUB)))
-                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, ISSUE_TRACKER_LINK))));
+                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, Refstrings.ISSUE_TRACKER_LINK))));
         event.player.addChatMessage(new ChatComponentText(BLUE + VISIT_DISCORD + " ")
-                .appendSibling(new ChatComponentText(BLUE + DISCORD_LINK)
+                .appendSibling(new ChatComponentText(BLUE + Refstrings.DISCORD_LINK)
                         .setChatStyle(new ChatStyle()
                                 .setChatHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ChatComponentText(YELLOW + CLICK_DISCORD)))
-                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, DISCORD_LINK)))));
+                                .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, Refstrings.DISCORD_LINK)))));
         event.player.addChatMessage(new ChatComponentText(GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------"));
     }
     // spotless:on
