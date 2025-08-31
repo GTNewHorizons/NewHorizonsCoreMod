10927f9300e99c0c6f8ba9684ccad87e530a4f2e
LazyFleshWasTaken <40587985+LazyFleshWasTaken@users.noreply.github.com>
2025-08-31 14:38:29 -0700

    Unnerf Botania for integration
    # Conflicts:
    #	src/main/java/com/dreammaster/scripts/ScriptForbiddenMagic.java

--- src/main/java/com/dreammaster/scripts/ScriptThaumcraft.java
+++ src/main/java/com/dreammaster/scripts/ScriptThaumcraft.java
@@ -631,11 +631,11 @@ public class ScriptThaumcraft implements IScriptLoader {
                 "def",
                 "ghi",
                 'a',
-                "screwStainlessSteel",
+                "screwSteeleaf",
                 'b',
                 "foilSilver",
                 'c',
-                "screwStainlessSteel",
+                "screwSteeleaf",
                 'd',
                 "foilSilver",
                 'e',
@@ -643,11 +643,11 @@ public class ScriptThaumcraft implements IScriptLoader {
                 'f',
                 "foilSilver",
                 'g',
-                "screwStainlessSteel",
+                "screwSteeleaf",
                 'h',
                 "foilSilver",
                 'i',
-                "screwStainlessSteel");
+                "screwSteeleaf");
         TCHelper.addResearchPage(
                 "CAP_silver",
                 new ResearchPage(TCHelper.findArcaneRecipe(getModItem(Thaumcraft.ID, "WandCap", 1, 5))));
