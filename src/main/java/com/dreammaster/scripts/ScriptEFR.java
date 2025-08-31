9d1f5f38fbc44cdb27a781234d77187380a386cc
LazyFleshWasTaken <40587985+LazyFleshWasTaken@users.noreply.github.com>
2025-08-30 21:25:04 -0700

    Fix deeslate stuff; chembalance.

--- src/main/java/com/dreammaster/scripts/ScriptEFR.java
+++ src/main/java/com/dreammaster/scripts/ScriptEFR.java
@@ -1257,17 +1257,16 @@ public class ScriptEFR implements IScriptLoader {
                 .duration(2 * MINUTES).eut(TierEU.RECIPE_MV).addTo(centrifugeRecipes);
 
         OreDictionary.registerOre("dustDeepslate", NHItemList.DeepslateDust.getIS(1));
-        
-                GTValues.RA.stdBuilder().itemInputs(NHItemList.MetamorphicMineralMixture.getIS(36))
-                .itemOutputs(
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Epidote, 9L), //gt
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Cordierite, 9L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Cobaltite, 6L), //gt
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Bismuthinite, 4L), //bart
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Datolite, 4L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Titanite, 4L)) //gt++
-                .duration(2 * MINUTES).eut(TierEU.RECIPE_EV).addTo(centrifugeRecipes);
 
+        GTValues.RA.stdBuilder().itemInputs(OrePrefixes.dust, MaterialsGTNH.MetamorphicMineralMixture, 36L)
+                .itemOutputs(
+                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Epidote, 9L), // gt
+                        GTOreDictUnificator.get(OrePrefixes.dust, MaterialsGTNH.Cordierite, 9L),
+                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Cobaltite, 6L), // gt
+                        GTOreDictUnificator.get(WerkstoffLoader.Bismuthinit.get(OrePrefixes.dust, 4)), // bart
+                        GTOreDictUnificator.get(OrePrefixes.dust, MaterialsGTNH.Datolite, 4L),
+                        GTOreDictUnificator.get(MaterialsOres.TITANITE.getDust(4))) // gt++
+                .duration(2 * MINUTES).eut(TierEU.RECIPE_EV).addTo(centrifugeRecipes);
 
         GTValues.RA.stdBuilder().itemInputs(OrePrefixes.dust, MaterialsGTNH.MetamorphicMineralMixture, 36L)
                 .itemOutputs(
