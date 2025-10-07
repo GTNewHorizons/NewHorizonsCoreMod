6c9a6ac062fba534a50452bb5f8b61e0555296d3
cubefury <thetarragondevteam@gmail.com>
2025-10-07 17:41:27 +0800

    spotless

--- src/main/java/com/dreammaster/scripts/ScriptVendingMachine.java
+++ src/main/java/com/dreammaster/scripts/ScriptVendingMachine.java
@@ -1,20 +1,20 @@
 package com.dreammaster.scripts;
 
-import gregtech.api.GregTechAPI;
-import net.minecraft.item.ItemStack;
-
 import static gregtech.api.enums.Mods.AppliedEnergistics2;
 import static gregtech.api.enums.Mods.Minecraft;
 import static gregtech.api.enums.Mods.VendingMachine;
 import static gregtech.api.util.GTModHandler.getModItem;
-import static gregtech.api.util.GTRecipeBuilder.MINUTES;
-import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 
 import java.util.Arrays;
 import java.util.List;
 
+import net.minecraft.item.ItemStack;
+
+import gregtech.api.GregTechAPI;
+
 @SuppressWarnings("deprecation")
 public class ScriptVendingMachine implements IScriptLoader {
+
     @Override
     public String getScriptName() {
         return "Vending Machine";
@@ -27,19 +27,20 @@ public class ScriptVendingMachine implements IScriptLoader {
 
     @Override
     public void loadRecipes() {
-        addShapedRecipe(getModItem(VendingMachine.ID, "multimachine.vendingmachine", 1, 0, missing),
-            getModItem(Minecraft.ID, "dispenser", 1, 0),
-            "craftingToolWrench",
-            getModItem(Minecraft.ID, "dispenser", 1, 0),
-            getModItem(Minecraft.ID, "stone_button", 1, 0),
-            new ItemStack(GregTechAPI.sBlockCasings11, 1, 0),
-            getModItem(Minecraft.ID, "stone_button", 1, 0),
-            "cableGt01Tin",
-            getModItem(Minecraft.ID, "chest", 1),
-            "cableGt01Tin"
-            );
-        addShapelessRecipe(getModItem(VendingMachine.ID, "hatch.vendinguplink.me", 1, 0, missing),
-            getModItem(AppliedEnergistics2.ID, "tile.BlockInterface", 1, 0, missing),
-            getModItem(Minecraft.ID, "dispenser", 1, 0));
+        addShapedRecipe(
+                getModItem(VendingMachine.ID, "multimachine.vendingmachine", 1, 0, missing),
+                getModItem(Minecraft.ID, "dispenser", 1, 0),
+                "craftingToolWrench",
+                getModItem(Minecraft.ID, "dispenser", 1, 0),
+                getModItem(Minecraft.ID, "stone_button", 1, 0),
+                new ItemStack(GregTechAPI.sBlockCasings11, 1, 0),
+                getModItem(Minecraft.ID, "stone_button", 1, 0),
+                "cableGt01Tin",
+                getModItem(Minecraft.ID, "chest", 1),
+                "cableGt01Tin");
+        addShapelessRecipe(
+                getModItem(VendingMachine.ID, "hatch.vendinguplink.me", 1, 0, missing),
+                getModItem(AppliedEnergistics2.ID, "tile.BlockInterface", 1, 0, missing),
+                getModItem(Minecraft.ID, "dispenser", 1, 0));
     }
 }
