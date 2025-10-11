6560be6a2a94b46288e80f5971b1207c23cfc13b
Wilhelm Schuster <dev@rot13.io>
2025-10-12 00:03:43 +0200

    Drop invalid compareTo in commands

--- src/main/java/com/dreammaster/command/CustomFuelsCommand.java
+++ src/main/java/com/dreammaster/command/CustomFuelsCommand.java
@@ -3,7 +3,7 @@ package com.dreammaster.command;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.command.ICommand;
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -13,12 +13,7 @@ import com.dreammaster.main.MainRegistry;
 
 import eu.usrv.yamcore.auxiliary.PlayerChatHelper;
 
-public class CustomFuelsCommand implements ICommand {
-
-    @Override
-    public int compareTo(Object arg0) {
-        return 0;
-    }
+public class CustomFuelsCommand extends CommandBase {
 
     @Override
     public String getCommandName() {
@@ -31,7 +26,7 @@ public class CustomFuelsCommand implements ICommand {
     }
 
     @Override
-    public List getCommandAliases() {
+    public List<String> getCommandAliases() {
         ArrayList<String> aliases = new ArrayList<>();
         aliases.add("cfuels");
         aliases.add("cfl");
@@ -41,40 +36,39 @@ public class CustomFuelsCommand implements ICommand {
     @Override
     public void processCommand(ICommandSender pCmdSender, String[] pArgs) {
         if (pArgs.length == 0) {
-            if (InGame(pCmdSender)) {
+            if (inGame(pCmdSender)) {
                 PlayerChatHelper.SendError(pCmdSender, "Syntax error. Type /customfuels help for help");
             } else {
                 PlayerChatHelper.SendPlain(pCmdSender, "[CFLS] Syntax error. Type /customfuels help for help");
             }
-            return;
         } else if ("help".equalsIgnoreCase(pArgs[0])) {
-            SendHelpToPlayer(pCmdSender);
+            sendHelpToPlayer(pCmdSender);
         } else if ("reload".equalsIgnoreCase(pArgs[0])) {
             boolean tFlag = MainRegistry.Module_CustomFuels.ReloadCustomFuels();
             if (!tFlag) {
-                if (!InGame(pCmdSender)) {
+                if (!inGame(pCmdSender)) {
                     PlayerChatHelper.SendPlain(pCmdSender, "[CFLS] Reload failed. Check your log for syntax errors");
                 } else {
                     PlayerChatHelper.SendWarn(pCmdSender, "Reload failed. Check your log for syntax errors");
                 }
             } else {
-                if (!InGame(pCmdSender)) {
+                if (!inGame(pCmdSender)) {
                     PlayerChatHelper.SendPlain(pCmdSender, "[CFLS] Reload done. New config is activated");
                 } else {
                     PlayerChatHelper.SendInfo(pCmdSender, "Reload done. New config is activated");
                 }
             }
         } else {
-            SendHelpToPlayer(pCmdSender);
+            sendHelpToPlayer(pCmdSender);
         }
     }
 
-    private boolean InGame(ICommandSender pCmdSender) {
+    private boolean inGame(ICommandSender pCmdSender) {
         return pCmdSender instanceof EntityPlayer;
     }
 
-    private void SendHelpToPlayer(ICommandSender pCmdSender) {
-        if (!InGame(pCmdSender)) {
+    private void sendHelpToPlayer(ICommandSender pCmdSender) {
+        if (!inGame(pCmdSender)) {
             PlayerChatHelper.SendPlain(pCmdSender, "[CFLS] Valid options are: reload");
         } else {
             PlayerChatHelper.SendInfo(pCmdSender, "/customfuels reload");
@@ -83,23 +77,10 @@ public class CustomFuelsCommand implements ICommand {
 
     @Override
     public boolean canCommandSenderUseCommand(ICommandSender pCommandSender) {
-        if (pCommandSender instanceof EntityPlayerMP) {
-            EntityPlayerMP tEP = (EntityPlayerMP) pCommandSender;
-            boolean tPlayerOpped = MinecraftServer.getServer().getConfigurationManager()
-                    .func_152596_g(tEP.getGameProfile());
-            return tPlayerOpped;
+        if (pCommandSender instanceof EntityPlayerMP tEP) {
+            return MinecraftServer.getServer().getConfigurationManager().func_152596_g(tEP.getGameProfile());
         } else {
             return pCommandSender instanceof MinecraftServer;
         }
-    }
-
-    @Override
-    public List addTabCompletionOptions(ICommandSender p_71516_1_, String[] p_71516_2_) {
-        return null;
-    }
-
-    @Override
-    public boolean isUsernameIndex(String[] p_82358_1_, int p_82358_2_) {
-        return false;
     }
 }
