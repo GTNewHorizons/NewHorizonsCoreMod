55aad15c6299484ca4a9a377f11f8beb6820c519
EnderProyects <enderproyects@gmail.com>
2025-10-23 02:04:54 +0200

    change

--- src/main/java/com/dreammaster/scripts/ScriptSGCraft.java
+++ src/main/java/com/dreammaster/scripts/ScriptSGCraft.java
@@ -9,28 +9,20 @@ import static gregtech.api.enums.Mods.GoodGenerator;
 import static gregtech.api.enums.Mods.OpenComputers;
 import static gregtech.api.enums.Mods.SGCraft;
 import static gregtech.api.enums.Mods.TecTech;
-import static gregtech.api.recipe.RecipeMaps.plasmaArcFurnaceRecipes;
 import static gregtech.api.util.GTModHandler.getModItem;
-import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 
 import java.util.Arrays;
 import java.util.List;
 
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.fluids.FluidRegistry;
-import net.minecraftforge.fluids.FluidStack;
 
 import com.dreammaster.gthandler.CustomItemList;
-import com.dreammaster.item.NHItemList;
 
 import fox.spiteful.avaritia.crafting.ExtremeCraftingManager;
 import goodgenerator.loader.Loaders;
-import gregtech.api.enums.GTValues;
 import gregtech.api.enums.ItemList;
 import gregtech.api.enums.Materials;
 import gregtech.api.enums.OrePrefixes;
-import gregtech.api.enums.TierEU;
-import gregtech.api.util.GTModHandler;
 import gregtech.api.util.GTOreDictUnificator;
 
 public class ScriptSGCraft implements IScriptLoader {
@@ -281,26 +273,6 @@ public class ScriptSGCraft implements IScriptLoader {
                 'o', ocInterface,
                 'u', universeFluidCell);
 
-        // Zero Point Module recycling
-
-        GTValues.RA.stdBuilder()
-                .itemInputs(ItemList.ZPM.get(1))
-                .itemOutputs(
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.NaquadahAlloy, 8L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.DraconiumAwakened, 32L),
-                        GTOreDictUnificator.get(OrePrefixes.plate, Materials.Firestone, 32L),
-                        GTOreDictUnificator.get(OrePrefixes.dust, Materials.Dilithium, 32L),
-                        GTOreDictUnificator.get(OrePrefixes.itemCasing, Materials.Ichorium, 16L),
-                        GTOreDictUnificator.get(OrePrefixes.plateDense, Materials.Ardite, 64L),
-                        GTModHandler.getModItem(GalacticraftAmunRa.ID, "item.baseItem", 4L,3),
-                        NHItemList.ChaoticDust.getIS(2),
-                        GTModHandler.getModItem(GalacticraftAmunRa.ID, "item.baseItem", 1L,26)
-                )
-                .outputChances(5000, 5000, 5000, 5000, 5000, 3000, 2000, 500, 250)
-                .fluidInputs(new FluidStack(FluidRegistry.getFluid("plasma.celestialtungsten"), 144))
-                .fluidOutputs(new FluidStack(FluidRegistry.getFluid("molten.tungsten"), 144))
-                .duration(50 * SECONDS).eut(TierEU.RECIPE_ZPM)
-                .addTo(plasmaArcFurnaceRecipes);
 
         // spotless:on
     }
