ecc3670a0b3d80ab356facd1c30d44ddcb2b75a5
chochem <40274384+chochem@users.noreply.github.com>
2025-12-17 17:41:55 +0000

    Dont scale up sun alloy recipe times (#1515)

--- src/main/java/com/dreammaster/gthandler/recipes/AlloySmelterRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/AlloySmelterRecipes.java
@@ -6,7 +6,6 @@ import static gregtech.api.enums.Mods.EnderIO;
 import static gregtech.api.enums.Mods.IndustrialCraft2;
 import static gregtech.api.recipe.RecipeMaps.alloySmelterRecipes;
 import static gregtech.api.util.GTModHandler.getModItem;
-import static gregtech.api.util.GTRecipeBuilder.MINUTES;
 import static gregtech.api.util.GTRecipeBuilder.SECONDS;
 
 import net.minecraft.init.Blocks;
@@ -246,32 +245,32 @@ public class AlloySmelterRecipes implements Runnable {
                 .itemInputs(
                         GTOreDictUnificator.get(OrePrefixes.plate, Materials.Sunnarium, 4L),
                         getModItem(IndustrialCraft2.ID, "itemPartIridium", 8, 0))
-                .itemOutputs(getModItem(AdvancedSolarPanel.ID, "asp_crafting_items", 1, 1)).duration(40 * SECONDS)
+                .itemOutputs(getModItem(AdvancedSolarPanel.ID, "asp_crafting_items", 1, 1)).duration(20 * SECONDS)
                 .eut(TierEU.RECIPE_LuV).addTo(alloySmelterRecipes);
         GTValues.RA.stdBuilder().requireMods(AdvancedSolarPanel)
                 .itemInputs(
                         getModItem(AdvancedSolarPanel.ID, "asp_crafting_items", 1, 1),
                         getModItem(AdvancedSolarPanel.ID, "asp_crafting_items", 8, 3))
-                .itemOutputs(getModItem(AdvancedSolarPanel.ID, "asp_crafting_items", 1, 4))
-                .duration(1 * MINUTES + 20 * SECONDS).eut(TierEU.RECIPE_ZPM).addTo(alloySmelterRecipes);
+                .itemOutputs(getModItem(AdvancedSolarPanel.ID, "asp_crafting_items", 1, 4)).duration(20 * SECONDS)
+                .eut(TierEU.RECIPE_ZPM).addTo(alloySmelterRecipes);
         GTValues.RA.stdBuilder().requireMods(AdvancedSolarPanel)
                 .itemInputs(
                         getModItem(AdvancedSolarPanel.ID, "asp_crafting_items", 1, 4),
                         GTOreDictUnificator.get(OrePrefixes.plateDense, Materials.Naquadria, 1L))
-                .itemOutputs(NHItemList.EnrichedNaquadriaSunnariumAlloy.getIS(1)).duration(1 * MINUTES + 40 * SECONDS)
+                .itemOutputs(NHItemList.EnrichedNaquadriaSunnariumAlloy.getIS(1)).duration(20 * SECONDS)
                 .eut(TierEU.RECIPE_UV).addTo(alloySmelterRecipes);
         GTValues.RA.stdBuilder()
                 .itemInputs(
                         NHItemList.EnrichedNaquadriaSunnariumAlloy.getIS(1),
                         GTOreDictUnificator.get(OrePrefixes.plateDense, Materials.Neutronium, 1L))
-                .itemOutputs(NHItemList.EnrichedNaquadriaNeutroniumSunnariumAlloy.getIS(1)).duration(2 * MINUTES)
+                .itemOutputs(NHItemList.EnrichedNaquadriaNeutroniumSunnariumAlloy.getIS(1)).duration(20 * SECONDS)
                 .eut(TierEU.RECIPE_UHV).addTo(alloySmelterRecipes);
         GTValues.RA.stdBuilder()
                 .itemInputs(
                         NHItemList.EnrichedNaquadriaNeutroniumSunnariumAlloy.getIS(1),
                         GTOreDictUnificator.get(OrePrefixes.plate, Materials.Bedrockium, 18))
-                .itemOutputs(NHItemList.EnrichedXSunnariumAlloy.getIS(1)).duration(2 * MINUTES + 20 * SECONDS)
-                .eut(TierEU.RECIPE_UEV).addTo(alloySmelterRecipes);
+                .itemOutputs(NHItemList.EnrichedXSunnariumAlloy.getIS(1)).duration(20 * SECONDS).eut(TierEU.RECIPE_UEV)
+                .addTo(alloySmelterRecipes);
     }
 
 }
