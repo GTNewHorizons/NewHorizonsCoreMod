89ac9bb7b1c8bd8dce104394e3b20af288a62dbf
UltraProdigy <187078471+UltraProdigy@users.noreply.github.com>
2025-12-22 03:28:36 -0500

    Remove Beet Recipe Conflict for 2.8.4 (#1521)

--- src/main/java/com/dreammaster/scripts/ScriptEFR.java
+++ src/main/java/com/dreammaster/scripts/ScriptEFR.java
@@ -203,6 +203,7 @@ public class ScriptEFR implements IScriptLoader {
         addShapelessRecipe(
                 ItemList.Color_00.get(1L),
                 GTModHandler.getModItem(EtFuturumRequiem.ID, "wither_rose", 1L, 0));
+        addShapelessRecipe(ItemList.Color_01.get(1L), GTModHandler.getModItem(EtFuturumRequiem.ID, "beetroot", 1L, 0));
 
         GTValues.RA.stdBuilder().itemInputs(getModItem(EtFuturumRequiem.ID, "cornflower", 1, 0))
                 .itemOutputs(ItemList.Color_04.get(2L)).duration(15 * SECONDS).eut(2).addTo(extractorRecipes);
@@ -210,6 +211,8 @@ public class ScriptEFR implements IScriptLoader {
                 .itemOutputs(ItemList.Color_15.get(2L)).duration(15 * SECONDS).eut(2).addTo(extractorRecipes);
         GTValues.RA.stdBuilder().itemInputs(getModItem(EtFuturumRequiem.ID, "wither_rose", 1, 0))
                 .itemOutputs(ItemList.Color_00.get(2L)).duration(15 * SECONDS).eut(2).addTo(extractorRecipes);
+        GTValues.RA.stdBuilder().itemInputs(getModItem(EtFuturumRequiem.ID, "beetroot", 1, 0))
+                .itemOutputs(ItemList.Color_01.get(2L)).duration(15 * SECONDS).eut(2).addTo(extractorRecipes);
 
         // Slabs
 
