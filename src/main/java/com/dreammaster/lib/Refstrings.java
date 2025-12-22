8e0e9034dd1dfe57280da139319b3e053cb65268
boubou19 <miisterunknown@gmail.com>
2025-12-22 22:56:46 +0100

    bump to 2.8.4

--- src/main/java/com/dreammaster/lib/Refstrings.java
+++ src/main/java/com/dreammaster/lib/Refstrings.java
@@ -5,7 +5,7 @@ public final class Refstrings {
     public static final String MODID = "dreamcraft";
     public static final String NAME = "GT: New Horizons";
     public static final String VERSION = BuildVersion.VERSION;
-    public static final String MODPACKPACK_VERSION = "2.8.3";
+    public static final String MODPACKPACK_VERSION = "2.8.4";
     public static final String CLIENTSIDE = "com.dreammaster.main.ClientProxy";
     public static final String SERVERSIDE = "com.dreammaster.main.CommonProxy";
     public static final String COLLECTIONID = "GTNewHorizons";
