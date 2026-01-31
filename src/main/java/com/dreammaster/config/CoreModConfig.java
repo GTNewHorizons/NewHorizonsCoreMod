e38b6fe6aeb6299ba8e146d2c9d242b7c7a313a7
nicksitnikov <nicksitnikov@gmail.com>
2026-01-31 15:18:54 -0600

    Remove unused config entries

--- src/main/java/com/dreammaster/config/CoreModConfig.java
+++ src/main/java/com/dreammaster/config/CoreModConfig.java
@@ -31,28 +31,8 @@ public class CoreModConfig extends ConfigManager {
     public boolean AvaritiaFixEnabled;
     public boolean MinetweakerFurnaceFixEnabled;
     public String[] SkullFireSwordEntityTargets;
-    public String[] BlacklistedTileEntiyClassNames;
-
-    // Deep Dark void miner configs.
-    public boolean DebugPrintAllOres;
-    public boolean DebugPrintAllWerkstoff;
-    public boolean DebugPrintAddedOres;
-    public boolean DebugPrintWerkstoff;
-    public String[] MaterialWeights;
-    public String[] WerkstoffWeights;
-    public String[] GTPPMaterialWeights;
 
     public OilGeneratorFix.OilConfig OilFixConfig;
-
-    // pollution stuff
-    public int pollutionThresholdAirFilter = 10000;
-    public float globalMultiplicator = 30f;
-    public float scalingFactor = 2.5f;
-    public float bonusByTierT1 = 1f;
-    public float bonusByTierT2 = 1.05f;
-    public float bonusByTierT3 = 1.1f;
-    public int usagesPerAbsorptionFilter = 30;
-    public float boostPerAbsorptionFilter = 2f;
 
     @Override
     protected void PreInit() {
@@ -72,19 +52,9 @@ public class CoreModConfig extends ConfigManager {
         AvaritiaFixEnabled = false;
         MinetweakerFurnaceFixEnabled = true;
 
-        BlacklistedTileEntiyClassNames = new String[] {
-                "com.rwtema.extrautils.tileentity.enderquarry.TileEntityEnderQuarry" };
         SkullFireSwordEntityTargets = new String[] { "net.minecraft.entity.monster.EntitySkeleton",
                 "galaxyspace.SolarSystem.planets.venus.entities.EntityEvolvedFireSkeleton",
                 "micdoodle8.mods.galacticraft.core.entities.EntityEvolvedSkeleton" };
-
-        DebugPrintAllOres = false;
-        DebugPrintAddedOres = false;
-        MaterialWeights = new String[] {};
-        WerkstoffWeights = new String[] {};
-        GTPPMaterialWeights = new String[] {};
-
-        pollutionThresholdAirFilter = 10000;
     }
 
     @Override
@@ -166,96 +136,8 @@ public class CoreModConfig extends ConfigManager {
                 "ModFixes.Avaritia",
                 SkullFireSwordEntityTargets,
                 "The Canonical Class-Name of the Entity");
-        BlacklistedTileEntiyClassNames = _mainConfig.getStringList(
-                "BlacklistedTileEntiyClassNames",
-                "Modules.Worldaccelerator",
-                BlacklistedTileEntiyClassNames,
-                "The Canonical Class-Names of TileEntities that should be ignored by the WorldAccelerator");
-
-        DebugPrintAllOres = _mainConfig.getBoolean(
-                "DebugPrintAllOres",
-                "DeepDarkVoidMiner",
-                DebugPrintAllOres,
-                "Set to true to enable logging of all valid ores. This is useful for debugging, or finding names to add to the weight config.");
-        DebugPrintAddedOres = _mainConfig.getBoolean(
-                "DebugPrintAddedOres",
-                "DeepDarkVoidMiner",
-                DebugPrintAddedOres,
-                "Set to true to enable logging of ores added to the Deep Dark void miner, with weights and metadata IDs. This is useful for debugging.");
-        MaterialWeights = _mainConfig.getStringList(
-                "MaterialWeights",
-                "DeepDarkVoidMiner",
-                MaterialWeights,
-                "List of GregTech material names to adjust weight. Example line: \"Aluminium : 0.3\". Intervening whitespace will be ignored. Use the debug options to get valid names. Use weight <= 0 to disable an ore entirely. Anything not specified in the list will have weight 1. See: gregtech.api.enums.Materials");
-        WerkstoffWeights = _mainConfig.getStringList(
-                "WerkstoffWeights",
-                "DeepDarkVoidMiner",
-                WerkstoffWeights,
-                "List of BartWorks material names to adjust weight. Example line: \"Bismutite : 0.3\". Intervening whitespace will be ignored. Use the debug options to get valid names. Use weight <= 0 to disable an ore entirely. Anything not specified in the list will have weight 1. See: bartworks.system.material.Werkstoff");
-        GTPPMaterialWeights = _mainConfig.getStringList(
-                "GTPPMaterialWeights",
-                "DeepDarkVoidMiner",
-                GTPPMaterialWeights,
-                "List of GT++ material names to adjust weight. Example line: \"Cerite : 0.3\". Intervening whitespace will be ignored. Use the debug options to get valid names. Use weight <= 0 to disable an ore entirely. Anything not specified in the list will have weight 1. See: gtPlusPlus.core.material.ORES");
 
         OilFixConfig = new OilGeneratorFix.OilConfig(_mainConfig);
-
-        pollutionThresholdAirFilter = _mainConfig.getInt(
-                "PollutionThresholdAirFilter",
-                "Pollution",
-                pollutionThresholdAirFilter,
-                0,
-                Integer.MAX_VALUE,
-                "the threshold of pollution above which the electric air filters will start to work");
-        globalMultiplicator = _mainConfig.getFloat(
-                "globalMultiplicator",
-                "Pollution",
-                globalMultiplicator,
-                0,
-                100,
-                "global multiplicator in this formula: globalMultiplicator * bonusByTier * mufflerAmount * turbineEfficiency * maintenanceEff * Floor(scalingFactor^effectiveTier). This gives the pollution cleaned by the electric air filter per second");
-        scalingFactor = _mainConfig.getFloat(
-                "scalingFactor",
-                "Pollution",
-                scalingFactor,
-                0,
-                100,
-                "scaling factor in this formula: globalMultiplicator * bonusByTier * mufflerAmount * turbineEfficiency * maintenanceEff * Floor(scalingFactor^effectiveTier). This gives the pollution cleaned by the electric air filter per second");
-        bonusByTierT1 = _mainConfig.getFloat(
-                "bonusByTierT1",
-                "Pollution",
-                bonusByTierT1,
-                0,
-                100,
-                "T1 bonus tier in this formula: globalMultiplicator * bonusByTier * mufflerAmount * turbineEfficiency * maintenanceEff * Floor(scalingFactor^effectiveTier). This gives the pollution cleaned by the electric air filter per second");
-        bonusByTierT2 = _mainConfig.getFloat(
-                "bonusByTierT2",
-                "Pollution",
-                bonusByTierT2,
-                0,
-                100,
-                "T2 bonus tier in this formula: globalMultiplicator * bonusByTier * mufflerAmount * turbineEfficiency * maintenanceEff * Floor(scalingFactor^effectiveTier). This gives the pollution cleaned by the electric air filter per second");
-        bonusByTierT3 = _mainConfig.getFloat(
-                "bonusByTierT3",
-                "Pollution",
-                bonusByTierT3,
-                0,
-                100,
-                "T3 bonus tier in this formula: globalMultiplicator * bonusByTier * mufflerAmount * turbineEfficiency * maintenanceEff * Floor(scalingFactor^effectiveTier). This gives the pollution cleaned by the electric air filter per second");
-        boostPerAbsorptionFilter = _mainConfig.getFloat(
-                "boostPerAbsorptionFilter",
-                "Pollution",
-                boostPerAbsorptionFilter,
-                1,
-                100,
-                "boost applied when a filter has been set in the electric air filter.");
-        usagesPerAbsorptionFilter = _mainConfig.getInt(
-                "usagesPerAbsorptionFilter",
-                "Pollution",
-                usagesPerAbsorptionFilter,
-                1,
-                100,
-                "Number of usage per absorption filter.");
     }
 
     @Override
