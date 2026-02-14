056ab91cb4313ae922a9457268c9e85032de1908
Auynonymous <auynaaubin@gmail.com>
2026-02-14 02:27:00 -0600

    change redstone amount from 4 to 20

--- src/main/java/com/dreammaster/gthandler/recipes/MixerRecipes.java
+++ src/main/java/com/dreammaster/gthandler/recipes/MixerRecipes.java
@@ -19,7 +19,10 @@ import static gregtech.api.enums.Mods.UniversalSingularities;
 import static gregtech.api.enums.Mods.Witchery;
 import static gregtech.api.recipe.RecipeMaps.mixerRecipes;
 import static gregtech.api.util.GTModHandler.getModItem;
-import static gregtech.api.util.GTRecipeBuilder.*;
+import static gregtech.api.util.GTRecipeBuilder.INGOTS;
+import static gregtech.api.util.GTRecipeBuilder.MINUTES;
+import static gregtech.api.util.GTRecipeBuilder.SECONDS;
+import static gregtech.api.util.GTRecipeBuilder.TICKS;
 import static gtPlusPlus.api.recipe.GTPPRecipeMaps.mixerNonCellRecipes;
 import static gtPlusPlus.core.material.MaterialsElements.STANDALONE.CHRONOMATIC_GLASS;
 
@@ -670,7 +673,7 @@ public class MixerRecipes implements Runnable {
                         GTOreDictUnificator.get(OrePrefixes.dust, Materials.RedSteel, 1L),
                         GTOreDictUnificator.get(OrePrefixes.dust, Materials.BlueSteel, 1L),
                         GTOreDictUnificator.get(OrePrefixes.dust, Materials.Naquadah, 1L))
-                .fluidInputs(Materials.Redstone.getMolten(4 * INGOTS)).circuit(4)
+                .fluidInputs(Materials.Redstone.getMolten(20 * INGOTS)).circuit(4)
                 .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.ElectrumFlux, 4L))
                 .duration(2 * SECONDS + 10 * TICKS).eut(900).addTo(mixerRecipes);
 
