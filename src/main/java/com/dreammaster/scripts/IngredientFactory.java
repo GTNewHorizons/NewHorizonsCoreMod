8002762f1aa66a2e0caa77d0570cf47f316c3961
nicksitnikov <nicksitnikov@gmail.com>
2026-03-13 02:30:31 -0500

    Remove another allocation and add Javadocs

--- src/main/java/com/dreammaster/scripts/IngredientFactory.java
+++ src/main/java/com/dreammaster/scripts/IngredientFactory.java
@@ -1,7 +1,6 @@
 package com.dreammaster.scripts;
 
 import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.JsonToNBT;
 import net.minecraft.nbt.NBTTagCompound;
@@ -15,62 +14,98 @@ public final class IngredientFactory {
 
     private static final boolean DEBUG = Boolean.parseBoolean(System.getProperty("dreamcraft.debug.recipe"));
 
-    private static @NotNull ItemStack invalidItem(String modId, String item) {
+    /**
+     * Handle what happens when an item cannot be found in the runtime environment.
+     *
+     * @param modId Mod ID of the mod the item is from
+     * @param name  Item registry name
+     * @return An ItemStack of `minecraft:fire` with a custom name containing the ID of the missing item. (if debug mode
+     *         is enabled.)
+     * @throws RuntimeException If debug mode is disabled.
+     */
+    private static @NotNull ItemStack invalidItem(String modId, String name) {
         if (DEBUG) {
             final var fire = new ItemStack(Blocks.fire);
-            fire.setStackDisplayName(EnumChatFormatting.RED + "Missing Item: " + modId + ":" + item);
+            fire.setStackDisplayName(EnumChatFormatting.RED + "Missing Item: " + modId + ":" + name);
             return fire;
         } else {
-            throw new RuntimeException("Could not find ItemStack: " + modId + ":" + item);
+            throw new RuntimeException("Could not find ItemStack: " + modId + ":" + name);
         }
     }
 
-    public static @NotNull ItemStack getModItem(String modId, String item) {
-        final var stack = GameRegistry.findItemStack(modId, item, 1);
-        if (stack == null) return invalidItem(modId, item);
+    /**
+     * Helper function to get a new ItemStack from another mod with a stack size of 1.
+     *
+     * @param modId Mod ID of the mod the item is from
+     * @param name  Item registry name
+     * @return Created ItemStack or a placeholder fire ItemStack if lookup fails & debug mode is enabled.
+     * @throws RuntimeException If the item is cannot be found and debug mode is disabled.
+     */
+    public static @NotNull ItemStack getModItem(String modId, String name) {
+        final var item = GameRegistry.findItem(modId, name);
+        if (item == null) return invalidItem(modId, name);
 
-        return stack;
+        return new ItemStack(item);
     }
 
-    public static @NotNull ItemStack getModItem(String modId, String item, int amount) {
-        final var stack = GameRegistry.findItemStack(modId, item, amount);
-        if (stack == null) return invalidItem(modId, item);
+    /**
+     * Helper function to get a new ItemStack from another mod with a specific stack size.
+     *
+     * @param modId  Mod ID of the mod the item is from
+     * @param name   Item registry name
+     * @param amount The amount of items in the ItemStack
+     * @return Created ItemStack or a placeholder fire ItemStack if lookup fails & debug mode is enabled.
+     * @throws RuntimeException If the item is cannot be found and debug mode is disabled.
+     */
+    public static @NotNull ItemStack getModItem(String modId, String name, int amount) {
+        final var item = GameRegistry.findItem(modId, name);
+        if (item == null) return invalidItem(modId, name);
 
-        return stack;
+        return new ItemStack(item, amount);
     }
 
-    public static @NotNull ItemStack getModItem(String modId, String item, int amount, int meta) {
-        final var stack = GameRegistry.findItemStack(modId, item, amount);
-        if (stack == null) return invalidItem(modId, item);
+    /**
+     * Helper function to get a new ItemStack from another mod with a specific stack size and damage value.
+     *
+     * @param modId  Mod ID of the mod the item is from
+     * @param name   Item registry name
+     * @param amount The amount of items in the ItemStack
+     * @param meta   The meta (damage value) of the items in the ItemStack
+     * @return Created ItemStack or a placeholder fire ItemStack if lookup fails & debug mode is enabled.
+     * @throws RuntimeException If the item is cannot be found and debug mode is disabled.
+     */
+    public static @NotNull ItemStack getModItem(String modId, String name, int amount, int meta) {
+        final var item = GameRegistry.findItem(modId, name);
+        if (item == null) return invalidItem(modId, name);
 
-        Items.feather.setDamage(stack, meta);
-        return stack;
+        return new ItemStack(item, amount, meta);
     }
-
-    private IngredientFactory() {}
 
     /**
      * Helper function to get a new ItemStack with specified damage (meta) value and NBT tags.
      *
-     * @param modId  Mod ID of an the item
-     * @param item   Item registry name
-     * @param amount Amount to get
-     * @param meta   the meta id of the item to look at.
+     * @param modId  Mod ID of the mod the item is from
+     * @param name   Item registry name
+     * @param amount The amount of items in the ItemStack
+     * @param meta   The meta (damage value) of the items in the ItemStack
      * @param nbt    NBT data that the created stack should get, in format that {@link NBTTagCompound#toString()}
      *               returns. Can be checked in-game with `/iih` command
-     * @return Created ItemStack or replacement stack
-     * @throws RuntimeException if the NBT string parsing fails
+     * @return Created ItemStack or a placeholder fire ItemStack if lookup fails & debug mode is enabled.
+     * @throws RuntimeException If the item is cannot be found and debug mode is disabled, or if the NBT string parsing
+     *                          fails.
      */
-    public static @NotNull ItemStack createItemStack(String modId, String item, int amount, int meta, String nbt) {
-        ItemStack s = GameRegistry.findItemStack(modId, item, amount);
-        if (s == null) return invalidItem(modId, item);
+    public static @NotNull ItemStack createItemStack(String modId, String name, int amount, int meta, String nbt) {
+        final var item = GameRegistry.findItem(modId, name);
+        if (item == null) return invalidItem(modId, name);
 
-        Items.feather.setDamage(s, meta);
+        final var stack = new ItemStack(item, amount, meta);
         try {
-            s.stackTagCompound = (NBTTagCompound) JsonToNBT.func_150315_a(nbt);
+            stack.setTagCompound((NBTTagCompound) JsonToNBT.func_150315_a(nbt));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return s;
+        return stack;
     }
+
+    private IngredientFactory() {}
 }
