0b9fc4bb3e79d62d34f69d4d39f4439dab9aae18
nicksitnikov <nicksitnikov@gmail.com>
2026-03-15 15:42:58 -0500

    Apply spotless

--- src/main/java/com/dreammaster/mixin/mixins/early/MixinMinecraft_PackIcon.java
+++ src/main/java/com/dreammaster/mixin/mixins/early/MixinMinecraft_PackIcon.java
@@ -9,7 +9,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import com.dreammaster.client.util.IconLoader;
-import com.dreammaster.config.CoreModConfig;
 import com.dreammaster.lib.Refstrings;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
