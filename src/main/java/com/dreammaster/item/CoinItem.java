05ca17d7c329cb083456f18f693e951c5bc4cfff
tiffit <10173431+tiffit@users.noreply.github.com>
2026-06-02 09:54:51 -0700

    spotlessApply

--- src/main/java/com/dreammaster/item/CoinItem.java
+++ src/main/java/com/dreammaster/item/CoinItem.java
@@ -1,11 +1,13 @@
 package com.dreammaster.item;
 
-import com.dreammaster.lib.Refstrings;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
 import net.minecraft.util.IIcon;
+
+import com.dreammaster.lib.Refstrings;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class CoinItem extends Item {
 
@@ -15,7 +17,7 @@ public class CoinItem extends Item {
     @SideOnly(Side.CLIENT)
     private IIcon coinTypeIcon;
 
-    public CoinItem(String coinType, int coinTier){
+    public CoinItem(String coinType, int coinTier) {
         this.coinType = coinType;
         this.coinTier = coinTier;
     }
@@ -36,7 +38,7 @@ public class CoinItem extends Item {
     @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister register) {
         itemIcon = register.registerIcon(Refstrings.MODID + ":coins/background/big" + coinTier);
-        if(coinType != null){
+        if (coinType != null) {
             coinTypeIcon = register.registerIcon(Refstrings.MODID + ":coins/icons/" + coinType + coinTier);
         }
     }
