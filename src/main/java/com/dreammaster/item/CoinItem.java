bfe39caa84648ee8d9f4e35a41b871e96a48a4e9
tiffit <10173431+tiffit@users.noreply.github.com>
2026-06-02 09:54:29 -0700

    sprinkle in some @SideOnly(Side.CLIENT)

--- src/main/java/com/dreammaster/item/CoinItem.java
+++ src/main/java/com/dreammaster/item/CoinItem.java
@@ -1,6 +1,8 @@
 package com.dreammaster.item;
 
 import com.dreammaster.lib.Refstrings;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
 import net.minecraft.util.IIcon;
@@ -10,6 +12,7 @@ public class CoinItem extends Item {
     private final String coinType;
     private final int coinTier;
 
+    @SideOnly(Side.CLIENT)
     private IIcon coinTypeIcon;
 
     public CoinItem(String coinType, int coinTier){
@@ -18,16 +21,19 @@ public class CoinItem extends Item {
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
     public IIcon getIconFromDamageForRenderPass(int damage, int pass) {
         return pass == 0 ? itemIcon : coinTypeIcon;
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
     public boolean requiresMultipleRenderPasses() {
         return coinType != null;
     }
 
     @Override
+    @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister register) {
         itemIcon = register.registerIcon(Refstrings.MODID + ":coins/background/big" + coinTier);
         if(coinType != null){
