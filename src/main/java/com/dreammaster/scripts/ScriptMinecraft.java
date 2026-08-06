3bb6e20672d621e665df0afc9cf92c13023e2672
Dream Master <dream-master@gmx.net>
2026-08-06 22:34:01 +0700

    sa+deps

--- src/main/java/com/dreammaster/scripts/ScriptMinecraft.java
+++ src/main/java/com/dreammaster/scripts/ScriptMinecraft.java
@@ -263,8 +263,8 @@ public class ScriptMinecraft implements IScriptLoader {
                 .circuit(1).itemOutputs(getModItem(Minecraft.ID, "fence_gate", 1, 0)).duration(15 * SECONDS)
                 .eut(TierEU.RECIPE_ULV).addTo(assemblerRecipes);
         GTValues.RA.stdBuilder().itemInputs(new OreDictItemStack("logWood", 2), new OreDictItemStack("plankWood", 2))
-                .itemOutputs(getModItem(Minecraft.ID, "chest", 1, 0)).duration(5 * SECONDS)
-                .eut(TierEU.RECIPE_LV).addTo(assemblerRecipes);
+                .itemOutputs(getModItem(Minecraft.ID, "chest", 1, 0)).duration(5 * SECONDS).eut(TierEU.RECIPE_LV)
+                .addTo(assemblerRecipes);
         GTValues.RA.stdBuilder()
                 .itemInputs(
                         GTOreDictUnificator.get(OrePrefixes.stick, Materials.Wood, 1L),
