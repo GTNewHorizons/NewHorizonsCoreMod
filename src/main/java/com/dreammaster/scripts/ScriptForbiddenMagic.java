2ad1d4aa23e4973d8055c715319d304261ad3875
boubou19 <miisterunknown@gmail.com>
2026-08-11 20:43:17 +0200

    Localize Lanthanides recipe outputs

--- src/main/java/com/dreammaster/scripts/ScriptForbiddenMagic.java
+++ src/main/java/com/dreammaster/scripts/ScriptForbiddenMagic.java
@@ -10,6 +10,7 @@ import static gregtech.api.enums.Mods.Botania;
 import static gregtech.api.enums.Mods.EnderIO;
 import static gregtech.api.enums.Mods.ForbiddenMagic;
 import static gregtech.api.enums.Mods.IndustrialCraft2;
+import static gregtech.api.enums.Mods.MagicBees;
 import static gregtech.api.enums.Mods.Minecraft;
 import static gregtech.api.enums.Mods.Thaumcraft;
 import static gregtech.api.enums.Mods.ThaumicTinkerer;
@@ -66,6 +67,7 @@ public class ScriptForbiddenMagic implements IScriptLoader {
                 EnderIO.ID,
                 ForbiddenMagic.ID,
                 IndustrialCraft2.ID,
+                MagicBees.ID,
                 Thaumcraft.ID,
                 ThaumicTinkerer.ID,
                 TinkerConstruct.ID,
@@ -91,7 +93,7 @@ public class ScriptForbiddenMagic implements IScriptLoader {
         ChiselHelper.registerOredict("blockNetherStar", "blockNetherStar");
 
         GTValues.RA.stdBuilder().itemInputs(new ItemStack(Items.emerald))
-                .itemOutputs(getModItem(ForbiddenMagic.ID, "FMResource", 9, 0)).duration(1 * MINUTES).eut(5)
+                .itemOutputs(getModItem(MagicBees.ID, "beeNugget", 9, 6)).duration(1 * MINUTES).eut(5)
                 .addTo(centrifugeRecipes);
         GTValues.RA.stdBuilder().itemInputs(getModItem(ForbiddenMagic.ID, "InkFlower", 1, 0))
                 .itemOutputs(ItemList.Color_00.get(2L)).duration(15 * SECONDS).eut(2).addTo(extractorRecipes);
