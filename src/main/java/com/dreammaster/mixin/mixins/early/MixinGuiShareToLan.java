28c091b725014dabb4a7887d097b29db4c5fd623
boubou19 <miisterunknown@gmail.com>
2026-08-12 18:28:46 +0200

    fix: append Open to LAN chat warning

--- src/main/java/com/dreammaster/mixin/mixins/early/MixinGuiShareToLan.java
+++ src/main/java/com/dreammaster/mixin/mixins/early/MixinGuiShareToLan.java
@@ -1,24 +1,26 @@
 package com.dreammaster.mixin.mixins.early;
 
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiShareToLan;
 import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.util.IChatComponent;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiShareToLan.class)
 public abstract class MixinGuiShareToLan extends GuiScreen {
 
-    @ModifyArg(
+    @Inject(
             method = "actionPerformed",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/client/gui/GuiNewChat;printChatMessage(Lnet/minecraft/util/IChatComponent;)V"))
-    public IChatComponent dreamcraft$changeShareToLANChatMessage(IChatComponent p_146227_1_) {
-
-        return new ChatComponentTranslation("dreamcraft.gui.sharetolan.message");
+                    target = "Lnet/minecraft/client/gui/GuiNewChat;printChatMessage(Lnet/minecraft/util/IChatComponent;)V",
+                    shift = At.Shift.AFTER))
+    private void dreamcraft$appendShareToLANChatMessage(GuiButton button, CallbackInfo ci) {
+        this.mc.ingameGUI.getChatGUI()
+                .printChatMessage(new ChatComponentTranslation("dreamcraft.gui.sharetolan.message"));
     }
 }
