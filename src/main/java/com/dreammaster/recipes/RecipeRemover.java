e632122ec38fc18af16c519a056d93670e262c9b
DreamYao520 <weilingyi520@gmail.com>
2026-09-16 17:28:35 +0800

    Remove legacy Extra Utilities recipes

--- src/main/java/com/dreammaster/recipes/RecipeRemover.java
+++ src/main/java/com/dreammaster/recipes/RecipeRemover.java
@@ -1006,6 +1006,7 @@ public class RecipeRemover {
         removeRecipeByOutputDelayed(getModItem(ExtraUtilities.ID, "conveyor", 1, 0));
         removeRecipeByOutputDelayed(getModItem(ExtraUtilities.ID, "filing", 1, 0));
         removeRecipeByOutputDelayed(getModItem(ExtraUtilities.ID, "filing", 1, 1));
+        removeRecipeByOutputDelayed(getModItem(ExtraUtilities.ID, "mini-soul", 1, 0));
         removeRecipeByOutputDelayed(getModItem(ExtraUtilities.ID, "watering_can", 1, 1));
         removeRecipeByOutputDelayed(getModItem(ExtraUtilities.ID, "watering_can", 1, 3));
         removeRecipeByOutputDelayed(getModItem(ExtraUtilities.ID, "magnumTorch", 1, 0));
