91209f62d15fc62549470776412808f141719eed
Auynonymous <auynaaubin@gmail.com>
2026-09-18 12:20:19 -0500

    why are you molten

--- src/main/java/com/dreammaster/scripts/ScriptNanochipRecipes.java
+++ src/main/java/com/dreammaster/scripts/ScriptNanochipRecipes.java
@@ -1054,7 +1054,7 @@ public class ScriptNanochipRecipes implements IScriptLoader {
                         CircuitComponent.OpticalMainframeRack.getFakeStack(1),
                         CircuitComponent.ProcessedPlateRhugnor.getFakeStack(2),
                         CircuitComponent.ProcessedChipAttoPIC.getFakeStack(8))
-                .fluidInputs(Materials.Space.getFluid(250)).itemOutputs(CircuitComponent.PicoCPU.getFakeStack(1))
+                .fluidInputs(Materials.Space.getMolten(250)).itemOutputs(CircuitComponent.PicoCPU.getFakeStack(1))
                 .duration(40 * SECONDS).eut(TierEU.RECIPE_UMV).addTo(nanochipEncasementWrapper);
 
         // Quantum CPU
@@ -1063,7 +1063,7 @@ public class ScriptNanochipRecipes implements IScriptLoader {
                         CircuitComponent.PicoCircuitRack.getFakeStack(1),
                         CircuitComponent.ProcessedPlateMetastableOganesson.getFakeStack(2),
                         CircuitComponent.ProcessedChipZeptoPIC.getFakeStack(8))
-                .fluidInputs(Materials.Space.getFluid(500)).itemOutputs(CircuitComponent.QuantumCPU.getFakeStack(1))
+                .fluidInputs(Materials.Space.getMolten(500)).itemOutputs(CircuitComponent.QuantumCPU.getFakeStack(1))
                 .duration(20 * SECONDS).eut(TierEU.RECIPE_UXV).addTo(nanochipEncasementWrapper);
 
         // Planck CPU
@@ -1072,7 +1072,7 @@ public class ScriptNanochipRecipes implements IScriptLoader {
                         CircuitComponent.RealizedQuantumCircuitRack.getFakeStack(1),
                         CircuitComponent.ProcessedPlateHexanite.getFakeStack(2),
                         CircuitComponent.ProcessedChipYoctoPIC.getFakeStack(8))
-                .fluidInputs(Materials.Space.getFluid(1_000)).itemOutputs(CircuitComponent.PlanckCPU.getFakeStack(1))
+                .fluidInputs(Materials.Space.getMolten(1_000)).itemOutputs(CircuitComponent.PlanckCPU.getFakeStack(1))
                 .duration(7 * SECONDS).eut(TierEU.RECIPE_MAX).addTo(nanochipEncasementWrapper);
     }
 
